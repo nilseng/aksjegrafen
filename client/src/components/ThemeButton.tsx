e8030ee67c2ed5d853409a6c2f5bc7be62254bfd
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-08-27 17:35:27 +0200

    adding lowering to theme button

--- client/src/components/ThemeButton.tsx
+++ client/src/components/ThemeButton.tsx
@@ -20,6 +20,10 @@ export const ThemeButton = ({ theme, setTheme }: IProps) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        boxShadow:
+          theme.id === "light"
+            ? "inset 0px 0px 4px 0px rgba(255,255,255,0.5)"
+            : "inset 0px 0px 4px 0px rgba(0,0,0,0.5)",
       }}
       onClick={() => {
         localStorage.setItem("theme", theme.id === "light" ? "dark" : "light");
