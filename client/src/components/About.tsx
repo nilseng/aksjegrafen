e13aa5c828f60a2a868d94beed970d4aaf1ed4b2
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-10-24 22:36:36 +0200

    adding links to about images

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -35,46 +35,63 @@ export const About = () => {
             bilde.
           </p>
         </div>
-        <div className="mt-5 p-4" style={{ ...theme.elevation }}>
-          <p>
-            Man kan for eksempel se hvilke selskaper store norske konsern eier -
-            her Telenor ASA:
-          </p>
-          <div className="d-flex justify-content-center py-4">
-            <img
-              className="about-image"
-              src={theme.id === "light" ? telenor_img_light : telenor_img_dark}
-              alt="Eksempel fra Aksjegrafen - Teodor eier PureOKRs AS"
-            />
-          </div>
+        <div className="text-center mt-5 p-4" style={{ ...theme.elevation }}>
+          <a
+            href="/graph?_id=61229167c79b48b7e3e0bc90"
+            style={{ textDecoration: "none", color: theme.text }}
+          >
+            <p className="m-0">
+              Man kan for eksempel se hvilke selskaper store norske konsern eier
+              - her Telenor ASA:
+            </p>
+            <div className="d-flex justify-content-center py-4">
+              <img
+                className="about-image"
+                src={
+                  theme.id === "light" ? telenor_img_light : telenor_img_dark
+                }
+                alt="Eksempel fra Aksjegrafen - Teodor eier PureOKRs AS"
+              />
+            </div>
+          </a>
         </div>
         <div className="mt-5 p-4" style={{ ...theme.lowering }}>
-          <p>
-            Eller finne ut hvordan kjendisinvestorer har organisert selskapene
-            sine:
-          </p>
-          <div className="d-flex justify-content-center py-4">
-            <img
-              className="about-image"
-              src={theme.id === "light" ? kjendis_light : kjendis_dark}
-              alt="Eksempel fra Aksjegrafen - Teodor eier PureOKRs AS"
-            />
-          </div>
+          <a
+            href="/graph?_id=61229164c79b48b7e3e049a5"
+            style={{ textDecoration: "none", color: theme.text }}
+          >
+            <p className="text-center m-0">
+              Eller finne ut hvordan kjendisinvestorer har organisert selskapene
+              sine:
+            </p>
+            <div className="d-flex justify-content-center py-4">
+              <img
+                className="about-image"
+                src={theme.id === "light" ? kjendis_light : kjendis_dark}
+                alt="Eksempel fra Aksjegrafen - Teodor eier PureOKRs AS"
+              />
+            </div>
+          </a>
         </div>
         <div className="mt-5 p-4" style={{ ...theme.elevation }}>
-          <p>
-            Aksjegrafen er utviklet av selskapet PureOKRs AS som eies av meg,
-            Teodor Nilseng Danielsen:
-          </p>
-          <div className="d-flex justify-content-center py-4">
-            <img
-              style={{ height: "360px" }}
-              src={
-                theme.id === "light" ? pureokrs_img_light : pureokrs_img_dark
-              }
-              alt="Eksempel fra Aksjegrafen - Teodor eier PureOKRs AS"
-            />
-          </div>
+          <a
+            href="/graph?_id=61229a9bc79b48b7e30c24c5"
+            style={{ textDecoration: "none", color: theme.text }}
+          >
+            <p className="text-center m-0">
+              Aksjegrafen er utviklet av selskapet PureOKRs AS som eies av meg,
+              Teodor Nilseng Danielsen:
+            </p>
+            <div className="d-flex justify-content-center py-4">
+              <img
+                style={{ height: "360px" }}
+                src={
+                  theme.id === "light" ? pureokrs_img_light : pureokrs_img_dark
+                }
+                alt="Eksempel fra Aksjegrafen - Teodor eier PureOKRs AS"
+              />
+            </div>
+          </a>
         </div>
       </div>
     </Container>
