30dd8a9acb3a3046f80e16127cf5481fa09c201f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-13 12:19:01 +0100

    refactor: extracts heading to separate component

---
+++ client/src/components/Heading.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { Container } from "react-bootstrap";
+import { AppContext } from "../App";
+
+export const Heading = () => {
+  const { theme } = useContext(AppContext);
+  return (
+    <Container className="rounded p-sm-5 p-4">
+      <p
+        className="small rounded p-4 m-0"
+        style={{ color: theme.text, borderColor: theme.primary, borderWidth: "1px", borderStyle: "solid" }}
+      >
+        I{" "}
+        <span style={{ color: theme.primary }} className="font-weight-bold">
+          Aksjegrafen
+        </span>{" "}
+        kan du søke opp aksjonærer og aksjeselskaper i Norge. Disse presenteres i en <i>graf</i> - eller nettverk om du
+        vil. Denne typen visualisering gjør at man enkelt kan få oversikt over komplekse eierskapsstrukturer.
+      </p>
+    </Container>
+  );
+};
