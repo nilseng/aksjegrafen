b753c509d9fa32284fe38149865e704ff9aca273
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-13 15:27:06 +0100

    feat: adds p and l component

---
+++ client/src/components/Graph/GraphModal/ProfitAndLoss.tsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import { AppContext } from "../../../App";
+import { IFinancials } from "../../../services/brregService";
+
+export const ProfitAndLoss = ({ pAndL }: { pAndL: IFinancials["resultatregnskapResultat"] }) => {
+  const { theme } = useContext(AppContext);
+
+  return (
+    <>
+      <p className="font-weight-bold my-1">Resultat</p>
+      <p className="small font-weight-bold m-0">Driftsinntekter</p>
+      <code style={{ color: theme.primary }} className="m-0">
+        {pAndL?.driftsresultat?.driftsinntekter?.sumDriftsinntekter?.toLocaleString(navigator?.language)}
+      </code>
+      <p className="small font-weight-bold m-0">Driftsresultat</p>
+      <code
+        style={{
+          color: pAndL?.driftsresultat?.driftsresultat > 0 ? theme.primary : theme.danger,
+        }}
+        className="m-0"
+      >
+        {pAndL?.driftsresultat?.driftsresultat?.toLocaleString(navigator?.language)}
+      </code>
+      {pAndL?.totalresultat && (
+        <>
+          <p className="small font-weight-bold m-0">Totalresultat</p>
+          <code
+            className="mb-0"
+            style={{
+              color: pAndL?.totalresultat > 0 ? theme.primary : theme.danger,
+            }}
+          >
+            {pAndL?.totalresultat?.toLocaleString(navigator?.language)}
+          </code>
+        </>
+      )}
+      {pAndL?.aarsresultat && (
+        <>
+          <p className="small font-weight-bold m-0">Årsresultat</p>
+          <code
+            className="mb-0"
+            style={{
+              color: pAndL?.aarsresultat > 0 ? theme.primary : theme.danger,
+            }}
+          >
+            {pAndL?.aarsresultat?.toLocaleString(navigator?.language)}
+          </code>
+        </>
+      )}
+    </>
+  );
+};
