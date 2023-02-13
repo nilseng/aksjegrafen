878cbb7e889c570a635970321e8724558c28403c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-13 15:32:08 +0100

    feat: improves styling of modal

--- client/src/components/Graph/GraphModal/GraphDetailsModal.tsx
+++ client/src/components/Graph/GraphModal/GraphDetailsModal.tsx
@@ -19,18 +19,18 @@ export const GraphDetailsModal = ({ entity, setEntity }: IProps) => {
   const brregInfo = useBrregEntityInfo(entity);
 
   return (
-    <div className="row d-flex justify-content-center position-absolute w-100 h-75 mt-5 mx-auto">
+    <div className="row d-flex justify-content-center position-absolute w-100 h-75 mt-5">
       <div
         className="col col-lg-8 col-11 h-100 w-100 p-4"
         style={{
           overflow: "scroll",
-          backgroundColor: theme.background,
+          backgroundColor: theme.backgroundSecondary,
           color: theme.text,
           ...theme.elevation,
         }}
       >
         <div className="d-flex justify-content-between align-items-center w-100 pb-4">
-          <div className="d-flex flex-wrap align-items-center">
+          <div className="d-flex flex-fill flex-wrap align-items-center justify-content-center">
             <p className="h4 mb-0 mr-3">{entity.name}</p>
             <p className="small align-middle h-100 mb-0" style={{ color: theme.muted }}>
               {entity.orgnr}
@@ -54,39 +54,44 @@ export const GraphDetailsModal = ({ entity, setEntity }: IProps) => {
         <div style={{ overflow: "scroll" }}>
           <Financials entity={entity} />
           <EntityRelationships entity={entity} />
-          {brregInfo?.organisasjonsform?.beskrivelse && (
-            <ModalInfo title="Organisasjonsform" value={brregInfo.organisasjonsform.beskrivelse} />
-          )}
-          {brregInfo?.hjemmeside && <ModalInfo title="Hjemmeside" value={brregInfo.hjemmeside} link={true} />}
-          {brregInfo?.stiftelsesdato && (
-            <ModalInfo
-              title="Stiftelsesdato"
-              value={new Date(brregInfo?.stiftelsesdato).toLocaleString("nb-NO", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
-            />
-          )}
-          {brregInfo?.registreringsdatoEnhetsregisteret && (
-            <ModalInfo
-              title="Registreringdato"
-              value={new Date(brregInfo?.registreringsdatoEnhetsregisteret).toLocaleString("nb-NO", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
-            />
-          )}
-          {(brregInfo?.registrertIMvaregisteret || brregInfo?.registrertIMvaregisteret === false) && (
-            <ModalInfo title="Momsregistrert" value={brregInfo.registrertIMvaregisteret ? "Ja" : "Nei"} />
-          )}
-          {brregInfo?.naeringskode1?.beskrivelse && (
-            <ModalInfo title="Type virksomhet (næringskode 1)" value={brregInfo?.naeringskode1.beskrivelse} />
-          )}
-          {(brregInfo?.underAvvikling || brregInfo?.underAvvikling === false) && (
-            <ModalInfo title="Under avvikling" value={brregInfo?.underAvvikling ? "Ja" : "Nei"} />
-          )}
+          <div className="p-4">
+            <p style={{ color: theme.primary }} className="font-weight-bold">
+              Annen informasjon om selskapet
+            </p>
+            {brregInfo?.organisasjonsform?.beskrivelse && (
+              <ModalInfo title="Organisasjonsform" value={brregInfo.organisasjonsform.beskrivelse} />
+            )}
+            {brregInfo?.hjemmeside && <ModalInfo title="Hjemmeside" value={brregInfo.hjemmeside} link={true} />}
+            {brregInfo?.stiftelsesdato && (
+              <ModalInfo
+                title="Stiftelsesdato"
+                value={new Date(brregInfo?.stiftelsesdato).toLocaleString("nb-NO", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              />
+            )}
+            {brregInfo?.registreringsdatoEnhetsregisteret && (
+              <ModalInfo
+                title="Registreringdato"
+                value={new Date(brregInfo?.registreringsdatoEnhetsregisteret).toLocaleString("nb-NO", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              />
+            )}
+            {(brregInfo?.registrertIMvaregisteret || brregInfo?.registrertIMvaregisteret === false) && (
+              <ModalInfo title="Momsregistrert" value={brregInfo.registrertIMvaregisteret ? "Ja" : "Nei"} />
+            )}
+            {brregInfo?.naeringskode1?.beskrivelse && (
+              <ModalInfo title="Type virksomhet (næringskode 1)" value={brregInfo?.naeringskode1.beskrivelse} />
+            )}
+            {(brregInfo?.underAvvikling || brregInfo?.underAvvikling === false) && (
+              <ModalInfo title="Under avvikling" value={brregInfo?.underAvvikling ? "Ja" : "Nei"} />
+            )}
+          </div>
         </div>
       </div>
     </div>
