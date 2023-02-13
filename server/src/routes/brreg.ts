9b95d56364b1a7a77fd6e484a93282f7c1f5c5dc
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-13 23:01:16 +0100

    fix: corrects http error code and removes error catch

--- server/src/routes/brreg.ts
+++ server/src/routes/brreg.ts
@@ -6,10 +6,10 @@ const router = express.Router();
 
 router.get("/financials", query("orgnr"), async (req, res) => {
   const orgnr = matchedData(req).orgnr;
-  const data = await axios({ method: "GET", url: `https://data.brreg.no/regnskapsregisteret/regnskap/${orgnr}` })
-    .then(({ data }) => data)
-    .catch((error) => ({ error }));
-  return data.error ? res.status(400).json(data) : res.status(200).json(data);
+  const data = await axios({ method: "GET", url: `https://data.brreg.no/regnskapsregisteret/regnskap/${orgnr}` }).then(
+    ({ data }) => data
+  );
+  return data ? res.json(data) : res.status(404).json(data);
 });
 
 export default router;
