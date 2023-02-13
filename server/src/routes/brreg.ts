d3a7272bec1dca660c8871bf221699dd782a42b6
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-13 23:01:51 +0100

    fix: applies async route wrapper

--- server/src/routes/brreg.ts
+++ server/src/routes/brreg.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 import express from "express";
 import { matchedData, query } from "express-validator";
+import { asyncRouter } from "../asyncRouter";
 
 const router = express.Router();
 
-router.get("/financials", query("orgnr"), async (req, res) => {
-  const orgnr = matchedData(req).orgnr;
-  const data = await axios({ method: "GET", url: `https://data.brreg.no/regnskapsregisteret/regnskap/${orgnr}` }).then(
-    ({ data }) => data
-  );
-  return data ? res.json(data) : res.status(404).json(data);
-});
+router.get(
+  "/financials",
+  query("orgnr"),
+  asyncRouter(async (req, res) => {
+    const orgnr = matchedData(req).orgnr;
+    const data = await axios({
+      method: "GET",
+      url: `https://data.brreg.no/regnskapsregisteret/regnskap/${orgnr}`,
+    }).then(({ data }) => data);
+    return data ? res.json(data) : res.status(404).json(data);
+  })
+);
 
 export default router;
