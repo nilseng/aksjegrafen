042f15d01c8af3d76e92ce389f5c2c076dcff52e
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-14 13:34:55 +0100

    feat: map visual

---
+++ client/src/components/MapVisual.tsx
@@ -0,0 +1,25 @@
+import { Map } from "mapbox-gl";
+import { useContext, useEffect, useRef } from "react";
+import { AppContext } from "../App";
+
+const createMap = (theme: string) => {
+  new Map({
+    container: "mapVisualContainer",
+    style: theme === "light" ? "mapbox://styles/mapbox/streets-v12" : "mapbox://styles/mapbox/dark-v11",
+    center: [10, 65],
+    zoom: 4.2,
+    accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+  });
+};
+
+export const MapVisual = () => {
+  const {
+    theme: { id: theme },
+  } = useContext(AppContext);
+
+  const containerRef = useRef(null);
+
+  useEffect(() => createMap(theme), [theme]);
+
+  return <div ref={containerRef} id="mapVisualContainer" className="h-100 w-100 rounded"></div>;
+};
