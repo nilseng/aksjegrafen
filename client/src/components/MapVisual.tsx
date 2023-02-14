b7aee0ead86f8857f1b5f22c2b8ec3f3e65e1295
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-14 16:51:33 +0100

    fix: sets style instead of recreating map when theme changes

--- client/src/components/MapVisual.tsx
+++ client/src/components/MapVisual.tsx
@@ -1,11 +1,10 @@
 import { Map as MapboxMap } from "mapbox-gl";
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AppContext } from "../App";
 
-const createMap = (theme: string, container: HTMLDivElement) => {
-  new MapboxMap({
+const createMap = (container: HTMLDivElement) => {
+  return new MapboxMap({
     container,
-    style: theme === "light" ? "mapbox://styles/mapbox/streets-v12" : "mapbox://styles/mapbox/dark-v11",
     center: [10, 65],
     zoom: 3.9,
     accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
@@ -19,10 +18,16 @@ export const MapVisual = () => {
 
   const mapContainer = useRef<HTMLDivElement>(null);
 
+  const [map, setMap] = useState<MapboxMap>();
+
   useEffect(() => {
     if (!mapContainer.current) return;
-    createMap(theme, mapContainer.current);
-  }, [theme]);
+    setMap(createMap(mapContainer.current));
+  }, []);
+
+  useEffect(() => {
+    map?.setStyle(theme === "light" ? "mapbox://styles/mapbox/streets-v12" : "mapbox://styles/mapbox/dark-v11");
+  }, [theme, map]);
 
   return <div ref={mapContainer} className="h-100 w-100 rounded"></div>;
 };
