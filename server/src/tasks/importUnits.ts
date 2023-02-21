a0ef98be7ae23c33566df3daf9c119ff3e877720
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-21 20:58:11 +0100

    chore: notes

--- server/src/tasks/importUnits.ts
+++ server/src/tasks/importUnits.ts
@@ -1,1 +1,10 @@
-export const importUnits = () => {};
+export const importUnits = () => {
+  /* 
+    1. Fetch all units from brreg
+    2. Stream the unit file content and save units to units collection or update company collection
+    */
+};
+
+const fetchUnits = () => {};
+
+const saveUnits = () => {};
