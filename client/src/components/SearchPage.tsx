208b07ffee09cb82bda310aeda35bc66a0192f0f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-03-16 14:37:08 +0100

    feat: unit search with paging

--- client/src/components/SearchPage.tsx
+++ client/src/components/SearchPage.tsx
@@ -1,8 +1,72 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../App";
+import { IBrregUnitResult, IBrregUnitSearchParams, searchBrregUnits } from "../services/brregService";
 
 export const SearchPage = () => {
   const { theme } = useContext(AppContext);
 
-  return <div style={{ color: theme.text }}></div>;
+  const [searchRes, setSearchRes] = useState<IBrregUnitResult>();
+
+  const handleSearch = async (searchParams?: IBrregUnitSearchParams) => {
+    const res = await searchBrregUnits(searchParams);
+    setSearchRes(res);
+  };
+
+  return (
+    <div style={{ color: theme.text }} className="container d-flex flex-column h-100 pb-5">
+      <h3 className="text-center">Søk i enhetsregisteret</h3>
+      <div>
+        <button
+          className="btn font-weight-bold m-4"
+          style={{ ...theme.button, color: theme.primary, minWidth: "8rem" }}
+          onClick={() => handleSearch()}
+        >
+          Søk
+        </button>
+      </div>
+      {searchRes && (
+        <div className="d-flex align-items-center pb-4 pl-4">
+          <button
+            className="btn font-weight-bold mr-4"
+            disabled={searchRes.page.number < 1}
+            style={{ ...theme.button, color: theme.primary, minWidth: "4rem" }}
+            onClick={() => handleSearch({ page: searchRes.page.number - 1 })}
+          >
+            Forrige
+          </button>
+          <p className="m-0">
+            Side{" "}
+            <span style={{ color: theme.primary }} className="font-weight-bold">
+              {searchRes.page.number + 1}
+            </span>{" "}
+            av{" "}
+            <span style={{ color: theme.primary }} className="font-weight-bold">
+              {searchRes.page.totalPages}
+            </span>
+          </p>
+          <button
+            className="btn font-weight-bold ml-4"
+            disabled={searchRes.page.number >= searchRes.page.totalElements - 1}
+            style={{ ...theme.button, color: theme.primary, minWidth: "4rem" }}
+            onClick={() => handleSearch({ page: searchRes.page.number + 1 })}
+          >
+            Neste
+          </button>
+        </div>
+      )}
+      <div className="flex-fill overflow-auto" style={{ ...theme.lowering }}>
+        {searchRes?._embedded.enheter.map((enhet) => (
+          <div key={enhet.organisasjonsnummer} style={{ ...theme.elevation }} className="m-4 p-4">
+            <p className="mb-0">{enhet.navn}</p>
+            <p className="p-0 m-0" style={{ color: theme.muted }}>
+              {enhet.organisasjonsnummer}
+            </p>
+            <p className="p-0 m-0" style={{ color: theme.muted }}>
+              {enhet.organisasjonsform.beskrivelse}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- client/src/services/brregService.ts
+++ client/src/services/brregService.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ICompany, IShareholder } from "../models/models";
+import { buildQuery } from "../utils/buildQuery";
 
 const brregUrl = "https://data.brreg.no/enhetsregisteret/api/enheter";
 
@@ -82,10 +83,84 @@ export interface IFinancials {
   };
 }
 
+export interface IBrregUnitResult {
+  _embedded: {
+    enheter: IBrregUnit[];
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
+  };
+  _links: {
+    first: {
+      href: string;
+    };
+    self: {
+      href: string;
+    };
+    next: {
+      href: string;
+    };
+    last: {
+      href: string;
+    };
+  };
+}
+
+interface IBrregUnit {
+  organisasjonsnummer: string;
+  navn: string;
+  organisasjonsform: {
+    kode: string;
+    beskrivelse: string;
+    _links: {
+      self: {
+        href: string;
+      };
+    };
+  };
+}
+
+const unitSearchParameters = {
+  navn: { type: "string", value: null },
+  organisasjonsnummer: { type: "string", value: null },
+  overordnetEnhet: { type: "string", value: null },
+  fraAntallAnsatte: { type: "number", value: null },
+  tilAntallAnsatte: { type: "number", value: null },
+  konkurs: { type: "boolean", value: null },
+  registrertIMvaregisteret: { type: "boolean", value: null },
+  registrertIForetaksregisteret: { type: "boolean", value: null },
+  registrertIStiftelsesregisteret: { type: "boolean", value: null },
+  registrertIFrivillighetsregisteret: { type: "boolean", value: null },
+  frivilligRegistrertIMvaregisteret: { type: "string", value: null },
+  underTvangsavviklingEllerTvangsopplosning: { type: "boolean", value: null },
+  underAvvikling: { type: "boolean", value: null },
+  /** Dato (ISO-8601): yyyy-MM-dd */
+  fraRegistreringsdatoEnhetsregisteret: { type: "string", value: null },
+  /** Dato (ISO-8601): yyyy-MM-dd */
+  tilRegistreringsdatoEnhetsregisteret: { type: "string", value: null },
+  /** Dato (ISO-8601): yyyy-MM-dd */
+  fraStiftelsesdato: { type: "string", value: null },
+  /** Dato (ISO-8601): yyyy-MM-dd */
+  tilStiftelsesdato: { type: "string", value: null },
+};
+
 export const getBrregUnit = async (orgnr: string) => {
   const res = await fetch(`${brregUrl}/${orgnr}`).catch(() =>
     console.warn(`Could not fetch info from brreg for company with orgnr=${orgnr}`)
   );
+  return res ? res.json() : res;
+};
+
+export interface IBrregUnitSearchParams {
+  page?: number;
+  navn?: string;
+}
+export const searchBrregUnits = async (searchParams?: IBrregUnitSearchParams): Promise<IBrregUnitResult> => {
+  const queryString = buildQuery(searchParams as { [key: string]: number | string });
+  const res = await fetch(`${brregUrl}/${queryString}`).catch(() => console.warn(`Failed to search brreg for units`));
   return res ? res.json() : res;
 };
 
