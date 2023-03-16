871cbb60a7b328777ce49135c750b7489f6e0d8c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-03-16 14:15:39 +0100

    refactor: cleans up build query util 🧹

--- client/src/utils/buildQuery.ts
+++ client/src/utils/buildQuery.ts
@@ -1,15 +1,8 @@
 import { isEmpty } from "lodash";
 
-export const buildQuery = (o: {
-  [key: string]: string | number | undefined;
-}) => {
-  let query = "";
-  if (isEmpty(o)) return query;
-  for (const key of Object.keys(o)) {
-    if (!o[key]) continue;
-    if (!query) query += "?";
-    else query += "&";
-    query += key + "=" + o[key];
-  }
-  return query;
+export const buildQuery = (o?: { [key: string]: string | number | undefined }) => {
+  if (!o || isEmpty(o)) return "";
+  return `?${Object.keys(o)
+    .map((key) => (o[key] ? `${key}=${o[key]}` : ""))
+    .join("&")}`;
 };
