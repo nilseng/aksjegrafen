980ebd9a7220ae036811c5fa434cb8abc850d74c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-03-21 14:26:53 +0100

    feat: improves styling of search results
    Also, shows message in case of no results.

--- client/src/components/SearchComponent.tsx
+++ client/src/components/SearchComponent.tsx
@@ -15,13 +15,18 @@ interface IProps {
   query?: { [key: string]: string | number };
 }
 
-export const SearchComponent = <T extends ICompany | IShareholder>({ type, placeholder, apiPath, query }: IProps) => {
+export const SearchComponent = <Entity extends ICompany | IShareholder>({
+  type,
+  placeholder,
+  apiPath,
+  query,
+}: IProps) => {
   const { theme } = useContext(AppContext);
   const history = useHistory();
 
   const [searchTerm, setSearchTerm] = useState<string>();
 
-  const searchList = useSearch<T[]>(apiPath, searchTerm, query);
+  const searchList = useSearch<Entity[]>(apiPath, searchTerm, query);
 
   const handleSearch = (e: any) => {
     setSearchTerm(e.target.value);
@@ -29,7 +34,7 @@ export const SearchComponent = <T extends ICompany | IShareholder>({ type, place
 
   return (
     <>
-      <Form.Group className="w-100 mt-5 px-3">
+      <Form.Group className="w-100 mt-5 mb-0 sm-px-3">
         <Form.Control
           name="selskapsSøk"
           autoComplete="off"
@@ -40,39 +45,64 @@ export const SearchComponent = <T extends ICompany | IShareholder>({ type, place
             borderColor: "transparent",
             color: theme.text,
             ...theme.lowering,
-            borderRadius: "4rem",
           }}
           onChange={handleSearch}
         ></Form.Control>
       </Form.Group>
-      <ListGroup className="w-100 mw-100" style={{ zIndex: 100 }}>
-        {searchList?.map((entity) => (
-          <ListGroup.Item
-            key={entity._id}
-            className="w-100 mw-100 d-flex align-items-center justify-content-between border-0 my-1"
-            style={{
-              backgroundColor: theme.background,
-              cursor: "pointer",
-              ...theme.elevation,
-            }}
-            onClick={() =>
-              history.push(type === "company" ? `/graph?_id=${entity._id}` : `/graph?shareholder_id=${entity._id}`)
-            }
+      {searchList && (
+        <div className="w-100 mw-100 px-0" style={{ maxHeight: "20rem" }}>
+          <div
+            className="h-100 w-100 position-relative p-3"
+            style={{ ...theme.elevation, backgroundColor: theme.background, zIndex: 100 }}
           >
-            <div>
-              <div className="mr-2">{entity.name}</div>
-              {entity.orgnr && <span className="small text-muted mr-3">{entity.orgnr}</span>}
-              {isShareholder(entity) && entity.yearOfBirth && (
-                <span className="small text-muted mr-2">{entity.yearOfBirth}</span>
+            <ListGroup
+              className="h-100 w-100 mw-100 overflow-auto position-relative px-3 py-2"
+              style={{ ...theme.lowering, zIndex: 102 }}
+            >
+              {searchList.length ? (
+                searchList.map((entity) => (
+                  <ListGroup.Item
+                    key={entity._id}
+                    className="w-100 mw-100 d-flex align-items-center justify-content-between border-0 my-2"
+                    style={{
+                      zIndex: 101,
+                      backgroundColor: "transparent",
+                      cursor: "pointer",
+                      ...theme.elevation,
+                    }}
+                    onClick={() =>
+                      history.push(
+                        type === "company" ? `/graph?_id=${entity._id}` : `/graph?shareholder_id=${entity._id}`
+                      )
+                    }
+                  >
+                    <div>
+                      <div className="mr-2">{entity.name}</div>
+                      {entity.orgnr && <span className="small text-muted mr-3">{entity.orgnr}</span>}
+                      {isShareholder(entity) && entity.yearOfBirth && (
+                        <span className="small text-muted mr-2">{entity.yearOfBirth}</span>
+                      )}
+                      {isShareholder(entity) && entity.countryCode && (
+                        <span className="small text-muted">{entity.countryCode}</span>
+                      )}
+                    </div>
+                    <FontAwesomeIcon
+                      icon={faSitemap}
+                      color={theme.primary}
+                      style={{ cursor: "pointer" }}
+                      className="mr-3"
+                    />
+                  </ListGroup.Item>
+                ))
+              ) : (
+                <p className="m-0" style={{ color: theme.primary }}>
+                  Ingen resultater 🔍
+                </p>
               )}
-              {isShareholder(entity) && entity.countryCode && (
-                <span className="small text-muted">{entity.countryCode}</span>
-              )}
-            </div>
-            <FontAwesomeIcon icon={faSitemap} color={theme.primary} style={{ cursor: "pointer" }} className="mr-3" />
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+            </ListGroup>
+          </div>
+        </div>
+      )}
     </>
   );
 };
