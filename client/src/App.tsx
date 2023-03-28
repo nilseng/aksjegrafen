bd669101e0a6f6236be8f7c1dd505c9da2cb9329
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-03-28 17:26:25 +0200

    feat: adds modal input context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Slide, ToastContainer } from "react-toastify";
 import { CytoGraph } from "./components/CytoGraph";
 import { EntityMap } from "./components/EntityMap";
 import { GraphContainer } from "./components/Graph/GraphContainer";
 import { Landing } from "./components/Landing";
 import NavBar from "./components/NavBar";
+import { Overlays } from "./components/Overlays";
 import { SearchPage } from "./components/SearchPage";
 import { Stats } from "./components/Stats";
+import { ICompany, IShareholder } from "./models/models";
 
 import { theming } from "./theming/theme";
 
@@ -16,12 +17,25 @@ export enum Theme {
   dark = "dark",
 }
 
-export const AppContext = React.createContext({
+interface IAppContext {
+  theme: typeof theming.light;
+  tableModalInput: {
+    investment?: ICompany;
+    setInvestment: Dispatch<SetStateAction<ICompany | undefined>>;
+    investor?: IShareholder;
+    setInvestor: Dispatch<SetStateAction<IShareholder | undefined>>;
+  };
+}
+
+export const AppContext = React.createContext<IAppContext>({
   theme: theming[Theme.light],
+  tableModalInput: { setInvestment: () => {}, setInvestor: () => {} },
 });
 
 const App = () => {
   const [theme, setTheme] = useState(theming[Theme.light]);
+  const [modalInvestment, setModalInvestment] = useState<ICompany>();
+  const [modalInvestor, setModalInvestor] = useState<IShareholder>();
 
   useEffect(() => {
     document.body.style.backgroundColor = theme.background;
@@ -33,35 +47,39 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
-      <NavBar theme={theme} setTheme={setTheme} />
-      <div
-        id="ag-main"
-        className="d-flex w-100 justify-content-center align-items-middle"
-        style={{
-          minHeight: "calc(100% - 83.2px)",
-          height: "calc(100% - 83.2px)",
-        }}
-      >
-        <Switch>
-          <AppContext.Provider value={{ theme }}>
+    <AppContext.Provider
+      value={{
+        theme,
+        tableModalInput: {
+          investment: modalInvestment,
+          setInvestment: setModalInvestment,
+          investor: modalInvestor,
+          setInvestor: setModalInvestor,
+        },
+      }}
+    >
+      <Router>
+        <NavBar theme={theme} setTheme={setTheme} />
+        <div
+          id="ag-main"
+          className="d-flex w-100 justify-content-center align-items-middle"
+          style={{
+            minHeight: "calc(100% - 83.2px)",
+            height: "calc(100% - 83.2px)",
+          }}
+        >
+          <Switch>
             <Route path="/" component={Landing} exact />
             <Route path="/graph" component={GraphContainer} />
             <Route path="/search" component={SearchPage} />
             <Route path="/stats" component={Stats} />
             <Route path="/cyto-graph" component={CytoGraph} />
             <Route path="/entity-map" component={EntityMap} />
-          </AppContext.Provider>
-        </Switch>
-      </div>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={4000}
-        transition={Slide}
-        hideProgressBar={true}
-        theme={theme.id === "dark" ? "dark" : "light"}
-      />
-    </Router>
+          </Switch>
+        </div>
+        <Overlays />
+      </Router>
+    </AppContext.Provider>
   );
 };
 
