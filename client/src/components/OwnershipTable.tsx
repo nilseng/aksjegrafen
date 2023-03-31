4161dcef383885fb52efaebd3bcb288cfc26ad8e
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-03-31 12:06:33 +0200

    feat: merges ownerships for the same ids

--- client/src/components/OwnershipTable.tsx
+++ client/src/components/OwnershipTable.tsx
@@ -1,23 +1,49 @@
-import { Fragment } from "react";
+import { Fragment, useMemo } from "react";
 import { IOwnership } from "../models/models";
-import { CopyButton } from "./CopyButton";
 
-export const OwnershipTable = ({ ownerships }: { ownerships: IOwnership[] }) => {
+interface MappedOwnership {
+  [key: string]: IOwnership & { [year: number]: number };
+}
+
+const getRowId = (type: "investor" | "investment", o: IOwnership) => {
+  return type === "investment" ? o.shareholderOrgnr ?? o.shareHolderId : o.orgnr;
+};
+
+export const OwnershipTable = ({ ownerships, type }: { ownerships: IOwnership[]; type: "investor" | "investment" }) => {
+  const mappedOwnerships = useMemo(() => {
+    const mappedOwnerships: MappedOwnership = {};
+    ownerships.forEach((o) => {
+      const mappedOwnership = mappedOwnerships[getRowId(type, o)];
+      mappedOwnerships[getRowId(type, o)] = {
+        ...mappedOwnership,
+        ...o,
+        [o.year]: (mappedOwnership?.[o.year] ?? 0) + +o.shareholderStocks,
+      };
+    });
+    return mappedOwnerships;
+  }, [ownerships, type]);
+
   return (
-    <div className="row py-2 py-sm-4 m-0">
+    <div className="row pt-3 m-0">
       <p className="col-6 font-weight-bold small">Navn</p>
-      <p className="col-3 font-weight-bold small">Antall aksjer</p>
-      <p className="col-3 font-weight-bold small">År</p>
-      {ownerships.map((i) => (
-        <Fragment key={i._id}>
-          <p className="col-6">{i.company?.name ?? i.shareholder?.name}</p>
-          <p className="col-3">
-            {(+i.shareholderStocks).toLocaleString()}
-            <CopyButton text={`${i.shareholderStocks}`} className="text-muted ml-1" />
-          </p>
-          <p className="col-3">{i.year}</p>
-        </Fragment>
-      ))}
+      <p className="col-6 font-weight-bold small">Antall aksjer</p>
+      <p className="col-6"></p>
+      <p className="col-2 small font-weight-bold">2021</p>
+      <p className="col-2 small font-weight-bold">2020</p>
+      <p className="col-2 small font-weight-bold">2019</p>
+      {Object.keys(mappedOwnerships)
+        .sort((a, b) => ((mappedOwnerships[a][2020] ?? 0) > (mappedOwnerships[b][2020] ?? 0) ? -1 : 1))
+        .sort((a, b) => ((mappedOwnerships[a][2021] ?? 0) > (mappedOwnerships[b][2021] ?? 0) ? -1 : 1))
+        .map((key) => (
+          <Fragment key={key}>
+            <p className="col-6">
+              {type === "investment" ? mappedOwnerships[key].shareholder?.name : mappedOwnerships[key].company?.name}
+            </p>
+            <p className="col-2">{mappedOwnerships[key][2021]?.toLocaleString()}</p>
+            <p className="col-2">{mappedOwnerships[key][2020]?.toLocaleString()}</p>
+            <p className="col-2">{mappedOwnerships[key][2019]?.toLocaleString()}</p>
+          </Fragment>
+        ))}
     </div>
   );
 };
