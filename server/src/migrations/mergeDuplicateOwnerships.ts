f9663e62de4ecbe03993fbf70e08231c86b57477
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-14 14:41:25 +0200

    chore: migration for marking duplicates as deleted

---
+++ server/src/migrations/mergeDuplicateOwnerships.ts
@@ -0,0 +1,22 @@
+import { IDatabase } from "../database/databaseSetup";
+
+export const mergeDuplicateOwnerships = async (db: IDatabase) => {
+  let count = 0;
+  console.log(`skipping ${count} items`);
+  const ownerships = db.ownerships.find({}).sort({ _id: -1 }).skip(count);
+  console.log("ownerships sorted and fetched");
+  ownerships.forEach(async (ownership) => {
+    count = count + 1;
+    console.log(count);
+    const dups = await db.ownerships
+      .find({
+        orgnr: ownership.orgnr,
+        shareHolderId: ownership.shareHolderId,
+      })
+      .sort({ year: -1, _id: -1 })
+      .toArray();
+    const firstId = dups[0]._id;
+    const ids = dups.map((o) => o._id).filter((_id) => _id !== firstId);
+    await db.ownerships.updateMany({ _id: { $in: ids } }, { $set: { deleted: true } });
+  });
+};
