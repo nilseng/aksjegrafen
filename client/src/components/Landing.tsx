a621be6736efaf1563601f44c788718c7e7e176a
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-15 13:39:05 +0200

    chore: removes disclaimer

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,4 +1,3 @@
-import { Container } from "react-bootstrap";
 import { About } from "./About";
 import { Disclaimer } from "./Disclaimer";
 import { Heading } from "./Heading";
@@ -8,13 +7,6 @@ export const Landing = () => {
   return (
     <div className="w-100">
       <div className="d-flex flex-column justify-content-center" style={{ minHeight: "100%" }}>
-        <Container className="p-4">
-          <h5 className="text-warning">Achtung!</h5>
-          <p className="border border-warning rounded small p-4">
-            Aksjegrafen gjennomgår en større endring i datamodellen, og unøyaktigheter må påregnes i et par dager, men
-            om litt kommer en forbedret versjon med nye kule features.
-          </p>
-        </Container>
         <SearchView />
         <Heading />
       </div>
