f92198dae0753dcf9a66a1e353e33ae35e26dce6
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-16 18:52:20 +0200

    chore: marks shareholders wo ownership for deletion

---
+++ server/src/migrations/deleteOldShareholders.ts
@@ -0,0 +1,19 @@
+import { BulkWriteOperation } from "mongodb";
+import { IDatabase } from "../database/databaseSetup";
+import { Shareholder } from "../models/models";
+
+export const deleteOldShareholders = async (db: IDatabase) => {
+  const shareholders = await db.shareholders.find({}).toArray();
+  const ownerships = await db.ownerships.find({}).toArray();
+  const ownershipMap: { [key: string]: true } = {};
+  ownerships.forEach((o) => {
+    ownershipMap[o.shareHolderId] = true;
+  });
+  const ops: BulkWriteOperation<Shareholder>[] = [];
+  for (const s of shareholders) {
+    if (!ownershipMap[s.id]) ops.push({ updateOne: { filter: { _id: s._id }, update: { $set: { deleted: true } } } });
+  }
+  const res = await db.shareholders.bulkWrite(ops);
+  console.log(res.matchedCount);
+  console.log(res.modifiedCount);
+};
