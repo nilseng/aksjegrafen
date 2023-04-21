4da7c05219c5f614900c5e6c42d856fe154f3b04
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-21 10:50:54 +0200

    feat: updates heading

--- client/src/components/Heading.tsx
+++ client/src/components/Heading.tsx
@@ -14,8 +14,9 @@ export const Heading = () => {
         <span style={{ color: theme.primary }} className="font-weight-bold">
           Aksjegrafen
         </span>{" "}
-        kan du søke opp alle aksjonærer og aksjeselskaper i Norge. Disse presenteres i en <i>graf</i> - eller nettverk
-        om du vil. Denne typen visualisering gjør at man enkelt kan få oversikt over komplekse eierskapsstrukturer.
+        kan du søke opp alle aksjonærer og aksjeselskaper i Norge. Naviger gjennom nettverket av investorer og
+        investeringer, se hvem som eier hvilke selskap, hvordan eierskapet har endret seg over tid, finansielle
+        nøkkeltall og informasjon fra enhetsregisteret.
       </p>
     </Container>
   );
