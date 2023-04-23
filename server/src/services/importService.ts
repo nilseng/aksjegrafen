9e45b8a4d39002c14b2fb43b659ee106c744078f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-23 21:38:47 +0200

    nit: import service comment

--- server/src/services/importService.ts
+++ server/src/services/importService.ts
@@ -4,4 +4,5 @@ import { Year } from "../models/models";
 export const importData = async (db: IDatabase, year?: Year, data?: (number | string)[]) => {
   //importShareholderRegistry(db, year, data);
   //importBusinessCodes(db);
+  //importRoles(db);
 };
