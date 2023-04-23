76d27bfae42e1b09cdef167cfe2d39b60a485642
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-23 21:39:13 +0200

    feat: role import

--- server/src/use-cases/importRoles.ts
+++ server/src/use-cases/importRoles.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import JSONStream from "JSONStream";
 import path from "path";
 import { IDatabase } from "../database/databaseSetup";
+import { Role } from "../models/models";
 
 export interface UnitRoles {
   organisasjonsnummer: string;
@@ -74,10 +75,33 @@ export const importRoles = (db: IDatabase) => {
   const stream = fs.createReadStream(
     path.join(__dirname, "../../..", "data", "roller_2023-04-21T06-45-04.308061.json")
   );
+  const roles: Role[] = [];
   stream.pipe(parser).on("data", (chunk: UnitRoles) => {
-    stream.destroy();
+    chunk.rollegrupper.forEach((group) => {
+      group.roller.forEach((r) => {
+        const role: Role = {
+          type: r.type.kode,
+          orgnr: chunk.organisasjonsnummer,
+          holder: {
+            person: {
+              fornavn: r.person?.navn.fornavn,
+              etternavn: r.person?.navn.etternavn,
+            },
+            unit: r.enhet
+              ? {
+                  orgnr: r.enhet?.organisasjonsnummer,
+                  organisasjonsform: r.enhet.organisasjonsform.kode,
+                  navn: r.enhet.navn[0],
+                }
+              : undefined,
+          },
+        };
+        roles.push(role);
+      });
+    });
   });
-  stream.on("end", () => {
-    console.log("Stream ended");
+  stream.on("end", async () => {
+    const res = await db.roles.insertMany(roles);
+    console.log(`Inserted ${res.insertedCount} roles`);
   });
 };
