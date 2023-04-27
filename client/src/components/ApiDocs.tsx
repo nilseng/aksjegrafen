a27131fa7d6299e99de6476bf3bc96181432a4f2
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-27 17:29:16 +0200

    feat: API docs examples

--- client/src/components/ApiDocs.tsx
+++ client/src/components/ApiDocs.tsx
@@ -30,15 +30,47 @@ export const ApiDocs = () => {
         title={"Søk etter selskap"}
         baseUrl={baseUrl}
         path={"/company"}
-        params={[{ name: "searchTerm", description: "organisasjonsnummer og/eller navn på selskapet" }]}
-        query={[{ name: "limit", description: "maks antall søkeresultater. Default er 10." }]}
+        params={[
+          { name: "searchTerm", example: "Telenor", description: "organisasjonsnummer og/eller navn på selskapet" },
+        ]}
+        query={[{ name: "limit", example: 1, description: "maks antall søkeresultater. Default er 10." }]}
+        exampleResponse={`
+[
+    {
+        "_id": "61229167c79b48b7e3e0bc90",
+        "orgnr": "982463718",
+        "name": "TELENOR ASA",
+        "stocks": 1399458033,
+        "investorCount": { "2019": 39472, "2020": 47080, "2021": 55667 },
+        "investmentCount": { "2019": 36, "2020": 14, "2021": 14 },
+        "shares": { "2019": { "total": 1442458032 }, "2020": { "total": 1399458033 }, "2021": { "total": 1399458033 } }
+    }
+]`}
       />
       <EndpointDescription
         title={"Søk etter aksjonær"}
         baseUrl={baseUrl}
         path={"/shareholder"}
-        params={[{ name: "searchTerm", description: "organisasjonsnummer og/eller navn på aksjonæren" }]}
-        query={[{ name: "limit", description: "maks antall søkeresultater. Default er 10." }]}
+        params={[
+          { name: "searchTerm", example: "Telenor", description: "organisasjonsnummer og/eller navn på aksjonæren" },
+        ]}
+        query={[{ name: "limit", example: 1, description: "maks antall søkeresultater. Default er 10." }]}
+        exampleResponse={`
+[
+    {
+        "_id": "61229192c79b48b7e3e461f2",
+        "id": "TELENOR ASA9824637181331 FORNEBUNOR",
+        "countryCode": "NOR",
+        "kind": 0,
+        "location": "FORNEBU",
+        "name": "TELENOR ASA",
+        "orgnr": "982463718",
+        "yearOfBirth": null,
+        "zipCode": "1331",
+        "investmentCount": { "2019": 36, "2020": 14, "2021": 14 }
+    }
+]
+          `}
       />
       <EndpointDescription
         title={"Hent investeringene til en aksjonær"}
@@ -47,6 +79,7 @@ export const ApiDocs = () => {
         query={[
           {
             name: "shareholderOrgnr",
+            example: "982463718",
             description: "organisasjonsnummeret i tilfelle aksjonæren er et selskap. (OPTIONAL)",
           },
           {
@@ -54,9 +87,32 @@ export const ApiDocs = () => {
             description:
               "id for aksjonæren i tilfelle aksjonæren er en privatperson, utenlandsk selskap e.l. (OPTIONAL)",
           },
-          { name: "limit", description: "maks antall søkeresultater. Default er 10." },
-          { name: "skip", description: "hopp over et antall investeringer. (OPTIONAL)" },
+          { name: "limit", example: 1, description: "maks antall søkeresultater. Default er 10." },
+          { name: "skip", example: 0, description: "hopp over et antall investeringer. (OPTIONAL)" },
         ]}
+        exampleResponse={`
+[
+    {
+        "_id": "627ed35bffdad219a1a401ea",
+        "orgnr": "985173710",
+        "shareHolderId": "TELENOR ASA9824637181331 FORNEBUNOR",
+        "shareholderOrgnr": "982463718",
+        "holdings": {
+            "2019": { "Ordinære aksjer": 659165, "total": 659165 },
+            "2020": { "Ordinære aksjer": 659165, "total": 659165 },
+            "2021": { "Ordinære aksjer": 659165, "total": 659165 }
+        },
+        "investment": {
+            "_id": "61229169c79b48b7e3e103b3",
+            "orgnr": "985173710",
+            "name": "TELENOR MARITIME AS",
+            "stocks": 666294,
+            "investorCount": { "2019": 2, "2020": 2, "2021": 2 },
+            "shares": { "2019": { "total": 666294 }, "2020": { "total": 666294 }, "2021": { "total": 666294 } }
+        }
+    }
+]
+          `}
       />
       <EndpointDescription
         title={"Hent aksjonærene i et selskap"}
@@ -65,11 +121,49 @@ export const ApiDocs = () => {
         query={[
           {
             name: "orgnr",
+            example: "985173710",
             description: "organisasjonsnummeret til selskapet.",
           },
-          { name: "limit", description: "maks antall søkeresultater. Default er 10." },
+          { name: "limit", example: 1, description: "maks antall søkeresultater. Default er 10." },
           { name: "skip", description: "hopp over et antall aksjonærer. (OPTIONAL)" },
         ]}
+        exampleResponse={`
+[
+    {
+        "_id": "627ed35bffdad219a1a401ea",
+        "orgnr": "985173710",
+        "shareHolderId": "TELENOR ASA9824637181331 FORNEBUNOR",
+        "shareholderOrgnr": "982463718",
+        "holdings": {
+            "2019": { "Ordinære aksjer": 659165, "total": 659165 },
+            "2020": { "Ordinære aksjer": 659165, "total": 659165 },
+            "2021": { "Ordinære aksjer": 659165, "total": 659165 }
+        },
+        "investor": {
+            "shareholder": {
+                "_id": "61229192c79b48b7e3e461f2",
+                "id": "TELENOR ASA9824637181331 FORNEBUNOR",
+                "countryCode": "NOR",
+                "kind": 0,
+                "location": "FORNEBU",
+                "name": "TELENOR ASA",
+                "orgnr": "982463718",
+                "yearOfBirth": null,
+                "zipCode": "1331",
+                "investmentCount": { "2019": 36, "2020": 14, "2021": 14 }
+            },
+            "company": {
+                "_id": "61229167c79b48b7e3e0bc90",
+                "orgnr": "982463718",
+                "name": "TELENOR ASA",
+                "stocks": 1399458033,
+                "investorCount": { "2019": 39472, "2020": 47080, "2021": 55667 },
+                "investmentCount": { "2019": 36, "2020": 14, "2021": 14 },
+                "shares": { "2019": { "total": 1442458032 }, "2020": { "total": 1399458033 }, "2021": { "total": 1399458033 } }
+            }
+        }
+    }
+]`}
       />
       <p>
         Har du en tilbakemelding, ønsker om endringer eller annen funksjonalitet? Send en mail til{" "}
--- client/src/components/EndpointDescription.tsx
+++ client/src/components/EndpointDescription.tsx
@@ -1,41 +1,100 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../AppContext";
 import { buildQuery } from "../utils/buildQuery";
 
 interface IProps {
   title: string;
   baseUrl: string;
   path: string;
-  params?: { name: string; description: string }[];
+  params?: { name: string; description: string; example?: string | number }[];
   query?: {
     name: string;
     description: string;
+    example?: string | number;
   }[];
+  exampleResponse: string;
 }
-export const EndpointDescription = ({ title, baseUrl, path, params, query }: IProps) => {
+export const EndpointDescription = ({ title, baseUrl, path, params, query, exampleResponse }: IProps) => {
+  const { theme } = useContext(AppContext);
+  const [activeTab, setActiveTab] = useState<"description" | "example">("description");
   return (
     <div className="pb-4">
       <p className="small font-weight-bold m-0">{title}</p>
-      <code>
-        {baseUrl}
-        {path}
-        {params ? `${params.map(({ name }) => `/{${name}}`)}` : ""}
-        {buildQuery(
-          (() => {
-            const o: { [key: string]: string } = {};
-            query?.forEach((q) => (o[q.name] = `{${q.name}}`));
-            return o;
-          })()
+      <div className="py-2">
+        <button
+          className="btn btn-sm"
+          style={
+            activeTab === "description"
+              ? { backgroundColor: theme.primary, color: theme.backgroundSecondary }
+              : { backgroundColor: theme.backgroundSecondary, color: theme.primary }
+          }
+          onClick={() => setActiveTab("description")}
+        >
+          Beskrivelse
+        </button>
+        {exampleResponse && (
+          <button
+            className="btn btn-sm"
+            style={
+              activeTab === "example"
+                ? { backgroundColor: theme.primary, color: theme.backgroundSecondary }
+                : { backgroundColor: theme.backgroundSecondary, color: theme.primary }
+            }
+            onClick={() => setActiveTab("example")}
+          >
+            Eksempel
+          </button>
         )}
-      </code>
-      {params?.map(({ name, description }) => (
-        <p className="m-0" key={name}>
-          <code>{name}</code> - {description}
-        </p>
-      ))}
-      {query?.map(({ name, description }) => (
-        <p className="m-0" key={name}>
-          <code>{name}</code> - {description}
-        </p>
-      ))}
+      </div>
+      {activeTab === "description" && (
+        <>
+          <code>
+            {baseUrl}
+            {path}
+            {params ? `${params.map(({ name }) => `/{${name}}`)}` : ""}
+            {buildQuery(
+              (() => {
+                const o: { [key: string]: string } = {};
+                query?.forEach((q) => (o[q.name] = `{${q.name}}`));
+                return o;
+              })()
+            )}
+          </code>
+          {params?.map(({ name, description }) => (
+            <p className="m-0" key={name}>
+              <code>{name}</code> - {description}
+            </p>
+          ))}
+          {query?.map(({ name, description }) => (
+            <p className="m-0" key={name}>
+              <code>{name}</code> - {description}
+            </p>
+          ))}
+        </>
+      )}
+      {exampleResponse && activeTab === "example" && (
+        <>
+          <p className="m-0">
+            <code>
+              {baseUrl}
+              {path}
+              {params ? `${params.map(({ example }) => `/${example}`)}` : ""}
+              {buildQuery(
+                (() => {
+                  const o: { [key: string]: string } = {};
+                  query?.forEach((q) => {
+                    if (q.example) o[q.name] = `${q.example}`;
+                  });
+                  return o;
+                })()
+              )}
+            </code>
+          </p>
+          <code>
+            <pre>{exampleResponse}</pre>
+          </code>
+        </>
+      )}
     </div>
   );
 };
