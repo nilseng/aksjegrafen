7d34ce3b37573df81da65614114069b538602443
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-04-27 11:17:23 +0200

    fix: aksjegrafen document title

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,9 +1,11 @@
+import { useDocumentTitle } from "../hooks/useDocumentTitle";
 import { About } from "./About";
 import { Disclaimer } from "./Disclaimer";
 import { Heading } from "./Heading";
 import { SearchView } from "./SearchView";
 
 export const Landing = () => {
+  useDocumentTitle("Aksjegrafen");
   return (
     <div className="w-100">
       <div className="d-flex flex-column justify-content-center" style={{ minHeight: "100%" }}>
