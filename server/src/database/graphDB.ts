c05432a4f4fc034d035446a1b8924b3853d1ac3f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-10 11:09:42 +0200

    chore: sets connection timeouts to 10 mins

--- server/src/database/graphDB.ts
+++ server/src/database/graphDB.ts
@@ -5,5 +5,5 @@ dotenv.config();
 export const graphDB = neo4j.driver(
   process.env.NEO4J_URL!,
   auth.basic(process.env.NEO4J_USERNAME!, process.env.NEO4J_PWD!),
-  { connectionTimeout: 1200_000 }
+  { connectionTimeout: 600_000, maxConnectionLifetime: 600_000, connectionAcquisitionTimeout: 600_000 }
 );
