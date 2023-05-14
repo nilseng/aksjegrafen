b52f1e5f789174b75fb2a73a63a2bc956258f702
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-14 18:14:26 +0200

    feat: provides redux store to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { store } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
