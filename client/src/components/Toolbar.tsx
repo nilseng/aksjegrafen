d797d6bfb3e82ac2d2d90f0405e4ab3069c4ddc8
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 16:23:53 +0200

    chore: toolbar to tw

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -6,7 +6,7 @@ import { AppContext } from "../AppContext";
 export const Toolbar = () => {
   const { theme } = useContext(AppContext);
   return (
-    <div className="btn position-absolute p-2 mr-2 mr-sm-4 mb-2 mb-sm-4" style={{ right: 0 }}>
+    <div className="absolute p-2 mr-2 sm:mr-4 mb-2 sm:mb-4" style={{ right: 0 }}>
       <button className="btn" style={{ backgroundColor: theme.backgroundSecondary, border: 0, ...theme.button }}>
         <FontAwesomeIcon size="lg" className="m-2" style={{ color: theme.primary }} icon={faSearch} />
       </button>
