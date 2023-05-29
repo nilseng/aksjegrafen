516e95495f9d554221f1d5d37110b354382918ab
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 16:52:21 +0200

    fix: attempts to fix input box shadow on mobile

--- client/src/theming/theme.ts
+++ client/src/theming/theme.ts
@@ -11,15 +11,15 @@ export const theming = {
     text: "#212529",
     muted: "#adb5bd",
     elevation: {
-      boxShadow: "-6px -6px 16px 0px rgba(255,255,255,0.5), 6px 6px 16px 0px rgba(209, 205, 199,0.5)",
+      boxShadow: "-6px -6px 16px 0px rgba(255,255,255, 0.5), 6px 6px 16px 0px rgba(209, 205, 199, 0.5)",
       borderRadius: 8,
     },
     lowering: {
-      boxShadow: "inset -3px -3px 8px 0px rgba(255,255,255,0.5), inset 3px 3px 8px 0px rgba(209, 205, 199,0.5)",
+      boxShadow: "inset -3px -3px 8px 0px rgba(255,255,255, 0.5), inset 3px 3px 8px 0px rgba(209, 205, 199, 0.5)",
       borderRadius: 8,
     },
     button: {
-      boxShadow: "inset -2px -2px 4px 0px rgba(209, 205, 199,0.5), inset 2px 2px 4px 0px rgba(255,255,255,0.5)",
+      boxShadow: "inset -2px -2px 4px 0px rgba(209, 205, 199, 0.5), inset 2px 2px 4px 0px rgba(255,255,255, 0.5)",
       borderRadius: 8,
     },
     primary: "#17a2b8",
@@ -40,15 +40,15 @@ export const theming = {
     text: "#f8f9fa",
     muted: "#868e96",
     elevation: {
-      boxShadow: "-4px -4px 8px 0px rgba(0, 0, 0,0.2), 4px 4px 8px 0px rgba(60, 60, 60,0.2)",
+      boxShadow: "-4px -4px 8px 0px rgba(0, 0, 0, 0.2), 4px 4px 8px 0px rgba(60, 60, 60, 0.2)",
       borderRadius: 8,
     },
     lowering: {
-      boxShadow: "inset -2px -2px 4px 0px rgba(0, 0, 0,0.2), inset 2px 2px 4px 0px rgba(60, 60, 60,0.2)",
+      boxShadow: "inset -2px -2px 4px 0px rgba(0, 0, 0, 0.2), inset 2px 2px 4px 0px rgba(60, 60, 60, 0.2)",
       borderRadius: 8,
     },
     button: {
-      boxShadow: "inset -2px -2px 4px 0px rgba(60, 60, 60,0.2), inset 2px 2px 4px 0px rgba(0, 0, 0,0.2)",
+      boxShadow: "inset -2px -2px 4px 0px rgba(60, 60, 60, 0.2), inset 2px 2px 4px 0px rgba(0, 0, 0, 0.2)",
       borderRadius: 8,
     },
     primary: "#17a2b8",
