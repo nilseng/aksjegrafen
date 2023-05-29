e6946d0a10b9d0ba4410a95671f00049aa00845c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 16:23:17 +0200

    chore: search view to tw

--- client/src/components/SearchView.tsx
+++ client/src/components/SearchView.tsx
@@ -1,8 +1,5 @@
 import { faList, faRoute, faUserTie } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import { useCompanyCount, useShareholderCount } from "../services/apiService";
 import { StatCard } from "./StatCard";
 
@@ -26,133 +23,119 @@ export const SearchView = () => {
   const shareholderCount = useShareholderCount();
 
   return (
-    <div className="d-flex w-100 justify-content-center align-items-middle">
-      <Container className="d-flex flex-column justify-content-between flex-fill" style={{ color: theme.text }}>
-        <div className="d-flex justify-content-center align-items-center flex-fill">
-          <Row className="w-100">
-            <Col
-              lg
-              className="d-flex flex-column align-items-center pt-5 m-sm-4 m-2"
-              style={{
-                backgroundColor: theme.background,
-                ...theme.elevation,
-                maxHeight: "14.5rem",
-                height: "14.5rem",
-              }}
-            >
-              <StatCard label="aksjeselskaper" labelIcon={faBuilding} stat={companyCount} />
-              <div className="w-100 mt-5 mb-0 sm-px-3">
-                <SearchComponent
-                  mapResultToListItem={(company: ICompany) => ({
-                    key: company._id,
-                    name: company.name,
-                    tags: company.orgnr ? [company.orgnr] : [],
-                    buttons: [
-                      {
-                        name: "table-button",
-                        buttonContent: <FontAwesomeIcon icon={faList} style={{ color: theme.primary }} size="lg" />,
-                        handleClick: (company: ICompany) => tableModalInput.setInvestment(company),
-                      },
-                      {
-                        name: "graph-button",
-                        buttonContent: (
-                          <span
-                            style={{
-                              ...theme.button,
-                              borderRadius: "100%",
-                              display: "inline-block",
-                              textAlign: "center",
-                              verticalAlign: "middle",
-                              width: "2.4rem",
-                              height: "2.4rem",
-                              paddingTop: "0.3rem",
-                              paddingBottom: "0.3rem",
-                            }}
-                          >
-                            <GraphLogo inputColor={theme.secondary} width={"1.5rem"} height={"1.5rem"} />
-                          </span>
-                        ),
-                        handleClick: (company: ICompany) => history.push(`/graph?_id=${company._id}`),
-                      },
-                      {
-                        name: "relation-finder-button",
-                        buttonContent: <FontAwesomeIcon icon={faRoute} style={{ color: theme.primary }} size="lg" />,
-                        handleClick: (company: ICompany) => history.push(`/relation-finder?_id=${company._id}`),
-                      },
-                    ],
-                  })}
-                  placeholder="Søk etter selskap..."
-                  apiPath={companySearchPath}
-                  query={searchQueryParams}
-                />
-              </div>
-            </Col>
-            <Col
-              lg
-              className="d-flex flex-column align-items-center pt-5 m-sm-4 m-2"
-              style={{
-                backgroundColor: theme.background,
-                ...theme.elevation,
-                maxHeight: "14.5rem",
-                height: "14.5rem",
-              }}
-            >
-              <StatCard label="aksjonærer" labelIcon={faUserTie} stat={shareholderCount} />
-              <div className="w-100 mt-5 mb-0 sm-px-3">
-                <SearchComponent
-                  mapResultToListItem={(shareholder: IShareholder) => ({
-                    key: shareholder._id,
-                    name: shareholder.name,
-                    tags: [shareholder.orgnr, shareholder.yearOfBirth, shareholder.countryCode].filter(
-                      (tag) => !!tag
-                    ) as (number | string)[],
-                    buttons: [
-                      {
-                        name: "table-button",
-                        buttonContent: <FontAwesomeIcon icon={faList} style={{ color: theme.primary }} size="lg" />,
-                        handleClick: (shareholder: IShareholder) => {
-                          tableModalInput.setInvestor(shareholder);
-                        },
-                      },
-                      {
-                        name: "graph-button",
-                        buttonContent: (
-                          <span
-                            style={{
-                              ...theme.button,
-                              borderRadius: "100%",
-                              display: "inline-block",
-                              textAlign: "center",
-                              verticalAlign: "middle",
-                              width: "2.4rem",
-                              height: "2.4rem",
-                              paddingTop: "0.3rem",
-                              paddingBottom: "0.3rem",
-                            }}
-                          >
-                            <GraphLogo inputColor={theme.secondary} width={"1.5rem"} height={"1.5rem"} />
-                          </span>
-                        ),
-                        handleClick: (shareholder: IShareholder) =>
-                          history.push(`/graph?shareholder_id=${shareholder._id}`),
-                      },
-                      {
-                        name: "relation-finder-button",
-                        buttonContent: <FontAwesomeIcon icon={faRoute} style={{ color: theme.primary }} size="lg" />,
-                        handleClick: (shareholder: IShareholder) =>
-                          history.push(`/relation-finder?shareholder_id=${shareholder._id}`),
-                      },
-                    ],
-                  })}
-                  placeholder="...eller aksjonær..."
-                  apiPath={shareholderSearchPath}
-                  query={searchQueryParams}
-                />
-              </div>
-            </Col>
-          </Row>
+    <div className="w-full flex justify-center flex-wrap dark:text-white">
+      <div
+        className="w-full max-w-lg flex flex-col justify-between items-center p-4 pt-12 m-4 sm:m-6"
+        style={{
+          backgroundColor: theme.background,
+          ...theme.elevation,
+          maxHeight: "14.5rem",
+          height: "14.5rem",
+        }}
+      >
+        <StatCard label="aksjeselskaper" labelIcon={faBuilding} stat={companyCount} />
+        <div className="w-full mt-12">
+          <SearchComponent
+            mapResultToListItem={(company: ICompany) => ({
+              key: company._id,
+              name: company.name,
+              tags: company.orgnr ? [company.orgnr] : [],
+              buttons: [
+                {
+                  name: "table-button",
+                  buttonContent: <FontAwesomeIcon icon={faList} style={{ color: theme.primary }} size="lg" />,
+                  handleClick: (company: ICompany) => tableModalInput.setInvestment(company),
+                },
+                {
+                  name: "graph-button",
+                  buttonContent: (
+                    <span
+                      className="flex justify-center items-center p-2"
+                      style={{
+                        ...theme.button,
+                        borderRadius: "100%",
+                      }}
+                    >
+                      <GraphLogo inputColor={theme.secondary} width={"1.5rem"} height={"1.5rem"} />
+                    </span>
+                  ),
+                  handleClick: (company: ICompany) => history.push(`/graph?_id=${company._id}`),
+                },
+                {
+                  name: "relation-finder-button",
+                  buttonContent: <FontAwesomeIcon icon={faRoute} style={{ color: theme.primary }} size="lg" />,
+                  handleClick: (company: ICompany) => history.push(`/relation-finder?_id=${company._id}`),
+                },
+              ],
+            })}
+            placeholder="Søk etter selskap..."
+            apiPath={companySearchPath}
+            query={searchQueryParams}
+          />
         </div>
-      </Container>
+      </div>
+      <div
+        className="w-full max-w-lg flex flex-col justify-between items-center p-4 pt-12 m-4 sm:m-6"
+        style={{
+          backgroundColor: theme.background,
+          ...theme.elevation,
+          maxHeight: "14.5rem",
+          height: "14.5rem",
+        }}
+      >
+        <StatCard label="aksjonærer" labelIcon={faUserTie} stat={shareholderCount} />
+        <div className="w-full mt-12">
+          <SearchComponent
+            mapResultToListItem={(shareholder: IShareholder) => ({
+              key: shareholder._id,
+              name: shareholder.name,
+              tags: [shareholder.orgnr, shareholder.yearOfBirth, shareholder.countryCode].filter((tag) => !!tag) as (
+                | number
+                | string
+              )[],
+              buttons: [
+                {
+                  name: "table-button",
+                  buttonContent: <FontAwesomeIcon icon={faList} style={{ color: theme.primary }} size="lg" />,
+                  handleClick: (shareholder: IShareholder) => {
+                    tableModalInput.setInvestor(shareholder);
+                  },
+                },
+                {
+                  name: "graph-button",
+                  buttonContent: (
+                    <span
+                      style={{
+                        ...theme.button,
+                        borderRadius: "100%",
+                        display: "inline-block",
+                        textAlign: "center",
+                        verticalAlign: "middle",
+                        width: "2.4rem",
+                        height: "2.4rem",
+                        paddingTop: "0.3rem",
+                        paddingBottom: "0.3rem",
+                      }}
+                    >
+                      <GraphLogo inputColor={theme.secondary} width={"1.5rem"} height={"1.5rem"} />
+                    </span>
+                  ),
+                  handleClick: (shareholder: IShareholder) => history.push(`/graph?shareholder_id=${shareholder._id}`),
+                },
+                {
+                  name: "relation-finder-button",
+                  buttonContent: <FontAwesomeIcon icon={faRoute} style={{ color: theme.primary }} size="lg" />,
+                  handleClick: (shareholder: IShareholder) =>
+                    history.push(`/relation-finder?shareholder_id=${shareholder._id}`),
+                },
+              ],
+            })}
+            placeholder="...eller aksjonær..."
+            apiPath={shareholderSearchPath}
+            query={searchQueryParams}
+          />
+        </div>
+      </div>
     </div>
   );
 };
