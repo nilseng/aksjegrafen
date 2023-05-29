4e3bd7bb5baa1598aa173ebd06034a96c7fcf6cf
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 23:38:50 +0200

    feat: search component in graph 2 modal

--- client/src/components/Graph2/Modal.tsx
+++ client/src/components/Graph2/Modal.tsx
@@ -2,6 +2,7 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch } from "react-redux";
 import { close } from "../../slices/modalSlice";
+import { SearchComponent } from "../SearchComponent";
 
 export const Modal = () => {
   const dispatch = useDispatch();
@@ -9,17 +10,16 @@ export const Modal = () => {
   return (
     <div className="absolute w-full h-full z-50 flex justify-center items-center">
       <div className="absolute w-full h-full" onClick={() => dispatch(close())}></div>
-      <div className="relative w-full sm:w-3/4 h-1/2 flex justify-center items-center bg-gray-50 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-white dark:border-gray-500 p-2 sm:p-4 m-2 sm:m-0">
-        <button>
-          <FontAwesomeIcon
-            className="absolute top-0 right-0 text-white m-2 sm:m-4"
-            icon={faTimes}
-            onClick={() => dispatch(close())}
-          />
+      <div className="relative w-full sm:w-3/4 max-w-2xl h-1/2 flex justify-center items-center bg-gray-50 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-white dark:border-gray-500 p-2 sm:p-4 m-2 sm:m-0">
+        <button className="absolute top-0 right-0 text-white m-2 sm:m-4">
+          <FontAwesomeIcon icon={faTimes} onClick={() => dispatch(close())} />
         </button>
-        <input
-          className="w-full md:w-3/4 rounded-xl focus:outline-none p-4"
+        <SearchComponent
+          inputContainerClassName="w-full md:w-3/4 rounded-lg bg-gray-50 dark:bg-gray-700"
+          inputClassName="ag-input focus:outline-none text-primary bg-transparent font-bold p-4"
           placeholder="Selskap, aksjonær eller rolleinnehaver..."
+          apiPath="/api/node"
+          mapResultToListItem={() => ({ key: "0", name: "", tags: [] })}
         />
       </div>
     </div>
