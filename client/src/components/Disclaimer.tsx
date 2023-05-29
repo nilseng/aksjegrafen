d71cb1449535f7b78d0a7b86caf2f99bbc2582ac
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 16:21:27 +0200

    chore: disclaimer to tw

--- client/src/components/Disclaimer.tsx
+++ client/src/components/Disclaimer.tsx
@@ -1,19 +1,14 @@
 import { useContext } from "react";
-import { Container } from "react-bootstrap";
 import { AppContext } from "../AppContext";
 
 export const Disclaimer = () => {
   const { theme } = useContext(AppContext);
   return (
-    <Container className="py-4">
-      <div className="w-100 p-3" style={theme.lowering}>
-        <div className="rounded p-3" style={{ border: `${theme.primary} solid 1px` }}>
-          <p className="small text-muted text-center m-0">
-            All data er hentet fra Skatteetatens aksjonærregister og Brønnøysundregistrenes APIer og er bearbeidet av
-            Aksjegrafen. Feil kan forekomme. Siden eies og er utviklet av Pureokrs AS (contact@pureokrs.com).
-          </p>
-        </div>
+    <div className="w-full flex justify-center p-5">
+      <div className="w-full max-w-3xl text-xs text-muted text-center p-5" style={theme.lowering}>
+        All data er hentet fra Skatteetatens aksjonærregister og Brønnøysundregistrenes APIer og er bearbeidet av
+        Aksjegrafen. Feil kan forekomme. Siden eies og er utviklet av Pureokrs AS (contact@pureokrs.com).
       </div>
-    </Container>
+    </div>
   );
 };
