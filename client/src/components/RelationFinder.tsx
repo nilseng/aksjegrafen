d5f37e58981e5749a52cf627e1f37a332fc9cb6b
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 16:22:37 +0200

    chore: relation finder to tw

--- client/src/components/RelationFinder.tsx
+++ client/src/components/RelationFinder.tsx
@@ -25,15 +25,15 @@ export const RelationFinder = () => {
 
   return (
     <div
-      className="d-flex flex-column align-items-center justify-content-between h-100 w-100 p-4"
+      className="flex flex-col items-center justify-between h-full w-full p-4"
       style={{ maxWidth: "750px", color: theme.text }}
     >
-      <h5 className="px-sm-5">
+      <h5 className="sm:px-5">
         <FontAwesomeIcon icon={faRoute} className="mr-2" style={{ color: theme.primary }} />
         Finn korteste vei fra <span style={{ color: theme.primary }}>{source?.name}</span> til{" "}
         <span style={{ color: theme.secondary }}>{target?.name ?? "?"}</span>
       </h5>
-      <div className="flex-fill w-100 p-sm-5">
+      <div className="grow w-full sm:p-5">
         <div style={{ height: "38px" }}>
           <SearchComponent
             mapResultToListItem={(company: ICompany) => ({
@@ -48,12 +48,12 @@ export const RelationFinder = () => {
           />
         </div>
         {isLoading && (
-          <div className="p-sm-5">
+          <div className="sm:p-5">
             <Loading color={theme.primary} backgroundColor={"transparent"} text={"Dette kan ta litt tid...🧘🏻‍♂️"} />
           </div>
         )}
         {path && path.length > 0 && (
-          <div className="p-2 p-sm-4" style={{ backgroundColor: theme.backgroundSecondary, borderRadius: "8px" }}>
+          <div className="p-2 sm:p-4" style={{ backgroundColor: theme.backgroundSecondary, borderRadius: "8px" }}>
             {path?.map((relation) => (
               <div
                 key={
@@ -61,7 +61,7 @@ export const RelationFinder = () => {
                   relation.ownership?.shareholderOrgnr ??
                   relation.ownership?.shareHolderId
                 }
-                className="w-100"
+                className="w-full py-2"
               >
                 {relation.role && (
                   <p>
@@ -69,7 +69,7 @@ export const RelationFinder = () => {
                       {relation.role.holder.unit?.name ?? relation.role.holder.unit?.navn}
                     </span>{" "}
                     er{" "}
-                    <span className="font-weight-bold">
+                    <span className="font-bold">
                       {roleTypes?.find((r) => r.kode === relation.role.type)?.beskrivelse ?? relation.role.type}
                     </span>{" "}
                     for{" "}
@@ -84,9 +84,7 @@ export const RelationFinder = () => {
                       {relation.ownership.investor?.company?.name ?? relation.ownership.investor?.shareholder.name}
                     </span>{" "}
                     eier{" "}
-                    <span className="font-weight-bold">
-                      {relation.ownership.holdings[2022]?.total.toLocaleString("NO")}
-                    </span>{" "}
+                    <span className="font-bold">{relation.ownership.holdings[2022]?.total.toLocaleString("NO")}</span>{" "}
                     aksjer i <span style={{ color: theme.secondary }}>{relation.ownership.investment?.name}</span>
                   </p>
                 )}
@@ -95,18 +93,18 @@ export const RelationFinder = () => {
           </div>
         )}
         {(!path || path.length === 0) && !isLoading && !error && target && (
-          <p className="w-100 text-center p-5 m-0" style={{ color: theme.primary }}>
+          <p className="w-full text-center p-5 m-0" style={{ color: theme.primary }}>
             Ingen relasjon funnet 🔍
           </p>
         )}
         {error && (
-          <p className="w-100 text-center p-5 m-0" style={{ color: theme.danger }}>
+          <p className="w-full text-center p-5 m-0" style={{ color: theme.danger }}>
             {error}
           </p>
         )}
       </div>
-      <div className="p-4" style={theme.lowering}>
-        Har du analyser eller større søk som du ønsker gjennomført? Ta kontakt på{" "}
+      <div className="text-sm p-4" style={theme.lowering}>
+        Har du analyser eller større søk du ønsker gjennomført? Ta kontakt på{" "}
         <span style={{ color: theme.primary }}>contact@pureokrs.com</span>.
       </div>
     </div>
