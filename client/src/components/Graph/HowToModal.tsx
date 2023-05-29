6806b7e859f4ac7419d75f1263c38b2a1cbb40cb
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-29 16:25:06 +0200

    chore: how to modal to tw

--- client/src/components/Graph/HowToModal.tsx
+++ client/src/components/Graph/HowToModal.tsx
@@ -11,18 +11,18 @@ export const HowToModal = () => {
 
   if (!isOpen)
     return (
-      <div
-        className="btn position-absolute p-0 mr-2 mr-sm-4 mb-2 mb-sm-4"
+      <button
+        className="absolute h-9 w-9 p-0 mr-4 mb-4"
         style={{ right: 0, bottom: 0 }}
         onClick={() => setIsOpen(true)}
       >
-        <FontAwesomeIcon className="m-2" style={{ color: theme.primary }} icon={faInfoCircle} />
-      </div>
+        <FontAwesomeIcon className="m-2" style={{ color: theme.primary }} icon={faInfoCircle} size="lg" />
+      </button>
     );
 
   return (
     <div
-      className="position-absolute h-75 w-75 mr-2 mr-sm-4 mb-2 mb-sm-4"
+      className="absolute h-3/4 md:h-1/2 w-75 md:w-1/2 mr-2 sm:mr-4 mb-2 sm:mb-4"
       style={{
         color: theme.text,
         right: 0,
@@ -32,10 +32,10 @@ export const HowToModal = () => {
         overflow: "scroll",
       }}
     >
-      <div className="btn float-right m-4" onClick={() => setIsOpen(false)}>
+      <button className="float-right m-4" onClick={() => setIsOpen(false)}>
         <FontAwesomeIcon icon={faTimes} style={{ color: theme.muted }} />
-      </div>
-      <h5 className="p-4">
+      </button>
+      <h5 className="text-xl font-bold p-4">
         <FontAwesomeIcon className="mr-4" style={{ color: theme.primary }} icon={faInfoCircle} />
         Forklaring
       </h5>
