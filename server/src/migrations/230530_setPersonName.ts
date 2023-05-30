ceec0221c859c1e0670cab1b18138fa5054bb70b
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-30 21:14:51 +0200

    chore: sets person full name

---
+++ server/src/migrations/230530_setPersonName.ts
@@ -0,0 +1,14 @@
+import { Driver } from "neo4j-driver";
+
+export const setPersonName = async (graphDB: Driver) => {
+  const session = graphDB.session();
+  const res = await session.run(`
+        CALL apoc.periodic.iterate(
+            "MATCH (p:Person) return p",
+            "SET p.name = p.firstName + ' ' + p.lastName",
+            {batchSize: 10000, paralell: true}
+        )
+    `);
+  console.log(res);
+  session.close();
+};
