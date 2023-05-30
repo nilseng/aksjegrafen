1f56c6d423e91c44770d354c7ffa1c78e6ee9388
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-30 21:59:53 +0200

    fix: revert

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -7,6 +7,7 @@ import { asyncRouter } from "../asyncRouter";
 import { IDatabase } from "../database/databaseSetup";
 import { Company, Ownership, Shareholder } from "../models/models";
 import { findShortestPath } from "../use-cases/findShortestPath";
+import { searchNode } from "../use-cases/searchNode";
 import { removeOrgnrWhitespace } from "../utils/removeOrgnrWhitespace";
 
 const router = Router();
@@ -322,7 +323,7 @@ export const api = ({ graphDB, mongoDB: db, cache }: { graphDB: Driver; mongoDB:
     })
   );
 
-  /* router.get(
+  router.get(
     "/node/:searchTerm",
     query(["limit"]).default(10).toInt(),
     asyncRouter(async (req, res) => {
@@ -330,7 +331,7 @@ export const api = ({ graphDB, mongoDB: db, cache }: { graphDB: Driver; mongoDB:
       const data = await searchNode({ searchTerm: req.params.searchTerm, graphDB, limit: query.limit });
       return res.status(200).json(data);
     })
-  ); */
+  );
 
   return router;
 };
