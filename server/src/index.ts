737553c568b481a33d675f36cb6b63c26a768233
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-05-30 22:16:25 +0200

    fix: tries disabling ssl redirect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
-import sslRedirect from "heroku-ssl-redirect";
 import morgan from "morgan";
 import { Driver } from "neo4j-driver";
 import path from "path";
@@ -30,7 +29,7 @@ const argv = yargs(hideBin(process.argv))
 
 const app = express();
 
-app.use(sslRedirect());
+// app.use(sslRedirect());
 
 app.use(
   bodyParser.urlencoded({
