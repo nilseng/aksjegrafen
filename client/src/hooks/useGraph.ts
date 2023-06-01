d22d3ce94482799024fd39785bfc551d80992635
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-01 17:03:24 +0200

    feat: applies graph slice in use graph hook

--- client/src/hooks/useGraph.ts
+++ client/src/hooks/useGraph.ts
@@ -1,21 +1,31 @@
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GraphState, fetchGraphThunk } from "../slices/graphSlice";
+import { AppDispatch, RootState } from "../store";
 import { useGraphQueryParams } from "./useGraphQueryParams";
 import { useNode } from "./useNode";
 
-export enum GraphType {
-  Default = "Default",
-  ShortestPath = "ShortestPath",
-  AllPaths = "AllPaths",
-}
-
 export const useGraph = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
   const { graphType, sourceId, targetId } = useGraphQueryParams();
 
-  const source = useNode(sourceId);
-  const target = useNode(targetId);
+  const { node: source } = useNode(sourceId);
+  const { node: target } = useNode(targetId);
 
   useEffect(() => {
-    if (graphType === GraphType.Default) {
+    if (source) {
+      dispatch(
+        fetchGraphThunk({
+          graphType,
+          sourceUuid: source?.properties.uuid,
+          targetUuid: target?.properties.uuid,
+          limit: 10,
+          skip: 0,
+        })
+      );
     }
-  }, [graphType]);
+  }, [dispatch, graphType, source, target?.properties.uuid]);
+
+  return useSelector<RootState, GraphState>((state) => state.graph);
 };
