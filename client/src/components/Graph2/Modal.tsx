2643bc7b4b253edf3e042271241e4d699965d572
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-05 17:25:26 +0200

    chore: adjusts blur and bg opacity of modal

--- client/src/components/Graph2/Modal.tsx
+++ client/src/components/Graph2/Modal.tsx
@@ -10,7 +10,7 @@ export const Modal = () => {
   return (
     <div className="absolute w-full h-full z-50 flex justify-center items-center">
       <div className="absolute w-full h-full" onClick={() => dispatch(close())}></div>
-      <div className="relative w-full sm:w-3/4 max-w-2xl h-1/2 flex justify-center items-center bg-gray-50 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 dark:bg-opacity-10 border border-white dark:border-gray-500 p-2 sm:p-4 m-2 sm:m-0">
+      <div className="relative w-full sm:w-3/4 max-w-2xl h-1/2 flex justify-center items-center bg-gray-50 dark:bg-gray-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-md dark:backdrop-blur-md bg-opacity-60 dark:bg-opacity-40 border border-white dark:border-gray-500 p-2 sm:p-4 m-2 sm:m-0">
         <NeuButton
           className="absolute top-0 right-0 h-12 w-12 p-2 m-2 sm:m-4"
           style={{ borderRadius: "100%" }}
