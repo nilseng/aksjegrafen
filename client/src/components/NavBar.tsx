a49b6701f25932ef500f2379c1340f0f25e6ad8b
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-05 10:10:24 +0200

    fix: uses react router dom link instead of history hook

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import "./NavBar.scss";
 
 import { faCode, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { theming } from "../theming/theme";
 import { GraphLogo } from "./GraphLogo";
 import { NeuButton } from "./NeuButton";
@@ -14,40 +14,42 @@ interface IProps {
 }
 
 const NavBar = ({ theme, setTheme }: IProps) => {
-  const history = useHistory();
   return (
     <div className="flex justify-between items-center p-4" style={{ zIndex: 10000 }}>
-      <NeuButton
-        className="h-12 w-12 p-2"
-        ariaLabel="Home"
-        style={{ borderRadius: "100%" }}
-        componentIcon={<GraphLogo inputColor={theme.primary} />}
-        action={() => history.push("/")}
-      />
+      <Link to="/" aria-label="Home">
+        <NeuButton
+          className="h-12 w-12 p-2"
+          ariaLabel="Home"
+          style={{ borderRadius: "100%" }}
+          componentIcon={<GraphLogo inputColor={theme.primary} />}
+        />
+      </Link>
       <div className="flex justify-end">
         <div className="flex items-center">
-          <NeuButton
-            className="p-2 mr-2"
-            ariaLabel="Avansert søk"
-            componentIcon={
-              <div className="text-xs text-primary font-bold">
-                Avansert søk
-                <FontAwesomeIcon icon={faSearch} className="ml-2" />
-              </div>
-            }
-            action={() => history.push("/search")}
-          />
-          <NeuButton
-            className="p-2 mr-2"
-            ariaLabel="API"
-            componentIcon={
-              <div className="text-xs text-primary font-bold">
-                API
-                <FontAwesomeIcon icon={faCode} className="ml-2" />
-              </div>
-            }
-            action={() => history.push("/api-docs")}
-          />
+          <Link to="/search" aria-label="Avansert søk">
+            <NeuButton
+              className="p-2 mr-2"
+              ariaLabel="Avansert søk"
+              componentIcon={
+                <div className="text-xs text-primary font-bold">
+                  Avansert søk
+                  <FontAwesomeIcon icon={faSearch} className="ml-2" />
+                </div>
+              }
+            />
+          </Link>
+          <Link to="/api-docs" aria-label="API Documentation">
+            <NeuButton
+              className="p-2 mr-2"
+              ariaLabel="API Documentation"
+              componentIcon={
+                <div className="text-xs text-primary font-bold">
+                  API
+                  <FontAwesomeIcon icon={faCode} className="ml-2" />
+                </div>
+              }
+            />
+          </Link>
           <NeuButton
             style={{ borderRadius: "4rem" }}
             componentIcon={<ThemeButton theme={theme} setTheme={setTheme} />}
