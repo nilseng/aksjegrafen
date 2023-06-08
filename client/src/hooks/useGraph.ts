e5f618e48d3ae3d1c25d748545f34c9dbcce1996
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-08 22:26:29 +0200

    chore: common loading state in use graph

--- client/src/hooks/useGraph.ts
+++ client/src/hooks/useGraph.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchState } from "../models/models";
 import { GraphState, fetchGraphThunk } from "../slices/graphSlice";
@@ -9,29 +9,35 @@ import { useNode } from "./useNode";
 export const useGraph = () => {
   const dispatch = useDispatch<AppDispatch>();
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const { graphType, sourceUuid, targetUuid } = useGraphQueryParams();
 
   const { node: source, isLoading: isLoadingSource } = useNode(sourceUuid);
   const { node: target, isLoading: isLoadingTarget } = useNode(targetUuid);
 
   useEffect(() => {
-    if (source) {
+    if (!isLoadingSource && !isLoadingTarget && source) {
       dispatch(
         fetchGraphThunk({
           graphType,
-          sourceUuid: source?.properties.uuid,
+          sourceUuid: source.properties.uuid,
           targetUuid: target?.properties.uuid,
           limit: 5,
           skip: 0,
         })
       );
     }
-  }, [dispatch, graphType, source, target?.properties.uuid]);
+  }, [dispatch, graphType, isLoadingSource, isLoadingTarget, source, target]);
 
   const { data, status, error } = useSelector<RootState, GraphState>((state) => state.graph);
 
+  useEffect(() => {
+    setIsLoading(isLoadingSource || isLoadingTarget || status === FetchState.Loading);
+  }, [isLoadingSource, isLoadingTarget, status]);
+
   return {
-    status: status === FetchState.Idle && (isLoadingSource || isLoadingTarget) ? FetchState.Loading : status,
+    status: isLoading ? FetchState.Loading : status,
     data: { ...data, source, target },
     error,
   };
