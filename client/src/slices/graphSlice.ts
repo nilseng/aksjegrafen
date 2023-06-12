64eb8cd6085447236034d1581a73f829008de181
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-12 17:33:01 +0200

    feat: fetches shortest path in graph slice

--- client/src/slices/graphSlice.ts
+++ client/src/slices/graphSlice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { SimulationLinkDatum, SimulationNodeDatum } from "d3";
 import { FetchState, GraphLink, GraphNode, GraphType } from "../models/models";
+import { buildQuery } from "../utils/buildQuery";
 
 export interface GraphState {
   data: {
@@ -54,7 +55,10 @@ function fetchGraph({
   skip: number;
 }) {
   if (graphType === GraphType.Default) return fetchNeighbours({ uuid: sourceUuid, limit, skip });
-  if (graphType === GraphType.ShortestPath) throw Error("Not implemented yet.");
+  if (graphType === GraphType.ShortestPath) {
+    fetchShortestPath({ sourceUuid, targetUuid, limit });
+    return { nodes: [], links: [] };
+  }
   if (graphType === GraphType.AllPaths) throw Error("Not implemented yet.");
   throw Error("Unknown graph type");
 }
@@ -69,6 +73,21 @@ async function fetchNeighbours({
   skip: number;
 }): Promise<{ nodes: GraphNode[]; links: GraphLink[] }> {
   const res = await fetch(`/api/graph/neighbours?uuid=${uuid}&limit=${limit}&skip=${skip}`);
+  return res.json();
+}
+
+async function fetchShortestPath({
+  sourceUuid,
+  targetUuid,
+  limit,
+}: {
+  sourceUuid: string;
+  targetUuid?: string;
+  limit?: number;
+}) {
+  if (!targetUuid) throw Error("Målnode ikke definert...");
+  const query = buildQuery({ sourceUuid, targetUuid, limit });
+  const res = await fetch(`/api/graph/shortest-path${query}`);
   return res.json();
 }
 
