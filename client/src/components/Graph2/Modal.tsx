7190fd68408ee2a578e23ac8d78dd20f30296c6c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-12 13:55:02 +0200

    refactor: creates target search component

--- client/src/components/Graph2/Modal.tsx
+++ client/src/components/Graph2/Modal.tsx
@@ -1,17 +1,14 @@
-import { faRoute, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { GraphNode, GraphType } from "../../models/models";
+import { GraphNode } from "../../models/models";
 import { ModalContent, close } from "../../slices/modalSlice";
 import { RootState } from "../../store";
 import { NeuButton } from "../NeuButton";
-import { SearchComponent } from "../SearchComponent";
 import { NodeSearch } from "./NodeSearch";
+import { TargetSearch } from "./TargetSearch";
 
 export const Modal = ({ source }: { source?: GraphNode }) => {
   const dispatch = useDispatch();
-  const history = useHistory();
 
   const content = useSelector<RootState, ModalContent>((state) => state.modalHandler.content);
 
@@ -27,51 +24,7 @@ export const Modal = ({ source }: { source?: GraphNode }) => {
           action={() => dispatch(close())}
         />
         {content === ModalContent.NodeSearch && <NodeSearch />}
-        {content === ModalContent.PathSearch && (
-          <div className="w-full flex flex-col justify-center items-center">
-            <p className="pb-4">
-              Finn relasjoner fra <span className="text-primary font-bold">{source?.properties.name}</span> til...
-            </p>
-            <div className="h-[2.375rem] w-full">
-              <SearchComponent
-                inputContainerClassName="w-full md:w-3/4 rounded-lg bg-gray-50 dark:bg-gray-700"
-                inputStyle={{
-                  backgroundColor: "transparent",
-                  backgroundClip: "padding-box",
-                  borderColor: "transparent",
-                }}
-                inputClassName="ag-input focus:outline-none text-primary dark:text-white bg-transparent font-bold p-4"
-                mapResultToListItem={(node: GraphNode) => ({
-                  key: node.properties.uuid,
-                  name: node.properties.name,
-                  tags: [],
-                  buttons: [
-                    {
-                      name: "shortest-path-button",
-                      condition: true,
-                      buttonContent: (
-                        <div>
-                          <FontAwesomeIcon icon={faRoute} className="text-primary" size="lg" />
-                          <p className="text-xs text-muted">korteste vei</p>
-                        </div>
-                      ),
-                      handleClick: (node: GraphNode) => {
-                        history.push({
-                          pathname: `/graph2`,
-                          search: `?graphType=${GraphType.ShortestPath}&sourceUuid=${source?.properties.uuid}&targetUuid=${node.properties.uuid}`,
-                        });
-                        dispatch(close());
-                      },
-                    },
-                  ],
-                })}
-                placeholder="Selskap, aksjonær eller rolleinnehaver..."
-                apiPath="/api/node"
-                query={{ limit: 10 }}
-              />
-            </div>
-          </div>
-        )}
+        {content === ModalContent.PathSearch && <TargetSearch source={source} />}
       </div>
     </div>
   );
---
+++ client/src/components/Graph2/TargetSearch.tsx
@@ -0,0 +1,59 @@
+import { faRoute } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { GraphNode, GraphType } from "../../models/models";
+import { close } from "../../slices/modalSlice";
+import { SearchComponent } from "../SearchComponent";
+
+export const TargetSearch = ({ source }: { source?: GraphNode }) => {
+  const history = useHistory();
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      <p className="pb-4">
+        Finn relasjoner fra <span className="text-primary font-bold">{source?.properties.name}</span> til...
+      </p>
+      <div className="h-[2.375rem] w-full">
+        <SearchComponent
+          inputContainerClassName="w-full md:w-3/4 rounded-lg bg-gray-50 dark:bg-gray-700"
+          inputStyle={{
+            backgroundColor: "transparent",
+            backgroundClip: "padding-box",
+            borderColor: "transparent",
+          }}
+          inputClassName="ag-input focus:outline-none text-primary dark:text-white bg-transparent font-bold p-4"
+          mapResultToListItem={(node: GraphNode) => ({
+            key: node.properties.uuid,
+            name: node.properties.name,
+            tags: [],
+            buttons: [
+              {
+                name: "shortest-path-button",
+                condition: true,
+                buttonContent: (
+                  <div>
+                    <FontAwesomeIcon icon={faRoute} className="text-primary" size="lg" />
+                    <p className="text-xs text-muted">korteste vei</p>
+                  </div>
+                ),
+                handleClick: (node: GraphNode) => {
+                  history.push({
+                    pathname: `/graph2`,
+                    search: `?graphType=${GraphType.ShortestPath}&sourceUuid=${source?.properties.uuid}&targetUuid=${node.properties.uuid}`,
+                  });
+                  dispatch(close());
+                },
+              },
+            ],
+          })}
+          placeholder="Selskap, aksjonær eller rolleinnehaver..."
+          apiPath="/api/node"
+          query={{ limit: 10 }}
+        />
+      </div>
+    </div>
+  );
+};
