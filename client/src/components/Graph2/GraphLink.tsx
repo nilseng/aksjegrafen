b759e45f6552c69b5564cd738ecf13a55394b635
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-13 16:09:52 +0200

    refactor: creates graph link component

---
+++ client/src/components/Graph2/GraphLink.tsx
@@ -0,0 +1,31 @@
+import { Fragment, useContext } from "react";
+import { AppContext } from "../../AppContext";
+import { GraphLink as IGraphLink } from "../../models/models";
+import { useRoleTypes } from "../../services/brregService";
+import { GraphNodeDatum } from "../../slices/graphSlice";
+
+export const GraphLink = ({ link }: { link: IGraphLink }) => {
+  const { theme } = useContext(AppContext);
+
+  const roleTypes = useRoleTypes();
+
+  return (
+    <Fragment
+      key={`${(link.source as GraphNodeDatum).properties.uuid}-${(link.target as GraphNodeDatum).properties.uuid}-${
+        link.type
+      }`}
+    >
+      <line className="graph-link" stroke={theme.primary} strokeWidth={1} />
+      <g className="graph-link-arrow">
+        <line x1={-5} y1={-5} x2={0} y2={0} stroke={theme.primary} strokeWidth="2" strokeLinecap="round" />
+        <line x1={0} y1={0} x2={5} y2={-5} stroke={theme.secondary} strokeWidth="2" strokeLinecap="round" />
+        <foreignObject width={200} height={50} transform={"translate(30, -24) rotate(90)"}>
+          <p className="font-bold text-primary">
+            {roleTypes?.find((role) => role.kode === link.type)?.beskrivelse ??
+              (link.type === "OWNS" ? "Eier" : link.type)}
+          </p>
+        </foreignObject>
+      </g>
+    </Fragment>
+  );
+};
