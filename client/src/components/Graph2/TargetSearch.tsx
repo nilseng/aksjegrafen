b7850da8252d72757d4d1092e212c8e986fa9967
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-14 12:41:18 +0200

    feat: buttons for specifying graph direction

--- client/src/components/Graph2/TargetSearch.tsx
+++ client/src/components/Graph2/TargetSearch.tsx
@@ -1,21 +1,52 @@
 import { faRoute } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { AppContext } from "../../AppContext";
+import { ReactComponent as DirectedGraphIcon } from "../../icons/directed_graph.svg";
+import { ReactComponent as UndirectGraphIcon } from "../../icons/undirected_graph.svg";
 import { GraphNode, GraphType } from "../../models/models";
 import { close } from "../../slices/modalSlice";
 import { SearchComponent } from "../SearchComponent";
 
 export const TargetSearch = ({ source }: { source?: GraphNode }) => {
+  const { theme } = useContext(AppContext);
+
   const history = useHistory();
 
   const dispatch = useDispatch();
+
+  const [isDirected, setIsDirected] = useState(true);
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <p className="pb-4">
         Finn relasjoner fra <span className="text-primary font-bold">{source?.properties.name}</span> til...
       </p>
+      <div className="w-full md:max-w-sm flex justify-around items-center pb-4">
+        <button
+          className="w-20 flex flex-col justify-center items-center p-1"
+          style={isDirected ? { ...theme.lowering } : { ...theme.button }}
+          onClick={() => setIsDirected(true)}
+        >
+          <div className="h-8">
+            <DirectedGraphIcon />
+          </div>
+          <p className="text-xs text-muted">rettet</p>
+        </button>
+        <button
+          className="w-20 flex flex-col justify-center items-center p-1"
+          style={isDirected ? { ...theme.button } : { ...theme.lowering }}
+          disabled={true}
+          /* onClick={() => setIsDirected(false)} */
+        >
+          <div className="h-8">
+            <UndirectGraphIcon />
+          </div>
+          <p className="text-xs text-muted">ikke rettet</p>
+        </button>
+      </div>
       <div className="h-[2.375rem] w-full md:max-w-sm">
         <SearchComponent
           inputContainerClassName="w-full rounded-lg bg-gray-50 dark:bg-gray-700"
