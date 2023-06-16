3b7216a00b4f2a086148af8bcf1e5c7fdecc40cf
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-16 23:37:50 +0200

    chore: removes unused limit

--- server/src/use-cases/findShortestPath2.ts
+++ server/src/use-cases/findShortestPath2.ts
@@ -1,11 +1,4 @@
 import { findShortestPath as shortestPathFinder } from "../gateways/neo4j/neo4j.gateway";
 
-export const findShortestPath = ({
-  sourceUuid,
-  targetUuid,
-  limit,
-}: {
-  sourceUuid: string;
-  targetUuid: string;
-  limit: number;
-}) => shortestPathFinder({ sourceUuid, targetUuid, limit });
+export const findShortestPath = ({ sourceUuid, targetUuid }: { sourceUuid: string; targetUuid: string }) =>
+  shortestPathFinder({ sourceUuid, targetUuid });
