a79793ea45cf36c93b5490b66a849fcfe31519b5
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-20 16:40:55 +0200

    chore: adds isDirected param to api

--- server/src/use-cases/findAllPaths.ts
+++ server/src/use-cases/findAllPaths.ts
@@ -1,6 +1,15 @@
 import { findAllPaths as allPathsFinder, findRelationships } from "../gateways/neo4j/neo4j.gateway";
+import { GraphLink, GraphNode } from "../models/models";
 
-export const findAllPaths = async (params: { sourceUuid: string; targetUuid: string; limit: number }) => {
+export const findAllPaths = async (params: {
+  isDirected?: boolean;
+  sourceUuid: string;
+  targetUuid: string;
+  limit: number;
+}): Promise<{
+  links: GraphLink[];
+  nodes: GraphNode[];
+}> => {
   const { links: pathLinks, nodes } = await allPathsFinder(params);
   const links = await findRelationships(pathLinks);
   return { links, nodes };
--- server/src/use-cases/findShortestPath2.ts
+++ server/src/use-cases/findShortestPath2.ts
@@ -1,4 +1,19 @@
-import { findShortestPath as shortestPathFinder } from "../gateways/neo4j/neo4j.gateway";
+import { findRelationships, findShortestPath as shortestPathFinder } from "../gateways/neo4j/neo4j.gateway";
+import { GraphLink, GraphNode } from "../models/models";
 
-export const findShortestPath = ({ sourceUuid, targetUuid }: { sourceUuid: string; targetUuid: string }) =>
-  shortestPathFinder({ sourceUuid, targetUuid });
+export const findShortestPath = async ({
+  isDirected,
+  sourceUuid,
+  targetUuid,
+}: {
+  isDirected?: boolean;
+  sourceUuid: string;
+  targetUuid: string;
+}): Promise<{
+  links: GraphLink[];
+  nodes: GraphNode[];
+}> => {
+  const { nodes, links: pathLinks } = await shortestPathFinder({ isDirected, sourceUuid, targetUuid });
+  const links = await findRelationships(pathLinks);
+  return { nodes, links };
+};
