2bae0e2ce3e47d62600b45abe0c99ce92c59c7c9
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-06-29 22:52:33 +0200

    chore: adds base url util

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -3,3 +3,8 @@ import { ICompany, IShareholder } from "../models/models";
 export const getIdentifier = (entity: ICompany | IShareholder) => {
   return entity?.orgnr ?? (entity as IShareholder)?.id;
 };
+
+export const getBaseUrl = () =>
+  window.location.hostname === "localhost"
+    ? `http://${window.location.hostname}:${window.location.port}`
+    : `https://${window.location.hostname}`;
