886147caefeca875734d7185fc80571cc333e738
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-07-01 19:04:19 +0200

    refactor: use graph menu hook

--- client/src/components/Graph2/GraphMenu/GraphMenu.tsx
+++ client/src/components/Graph2/GraphMenu/GraphMenu.tsx
@@ -1,16 +1,10 @@
-import { faListAlt } from "@fortawesome/free-regular-svg-icons";
-import { faDollarSign, faList, faWindowRestore, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { useContext, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { AppContext } from "../../../AppContext";
 import { useWindowDimensions } from "../../../hooks/useWindowDimensions";
-import { GraphNode, GraphNodeLabel, GraphType } from "../../../models/models";
-import { closeMenu, setSource } from "../../../slices/graphSlice";
-import { ModalContent, open as openModal, setContent } from "../../../slices/modalSlice";
-import { getBaseUrl } from "../../../utils/utils";
-import { GraphLogo } from "../../GraphLogo";
+import { GraphNode } from "../../../models/models";
 import { GraphMenuItem } from "./GraphMenuItem";
+import { useGraphMenu } from "./useGraphMenu";
 
 export interface MenuItem {
   node: GraphNode;
@@ -30,14 +24,10 @@ export interface Menu {
 }
 
 export const GraphMenu = ({ open, node, x, y }: Menu) => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-
   const { theme } = useContext(AppContext);
   const { width, height } = useWindowDimensions();
 
   const [pos, setPos] = useState<{ x: number; y: number }>();
-  const [menuItems, setMenuItems] = useState<MenuItem[]>();
 
   // If there is not enough space to display the ctx menu to the right/below clicked item,
   // adjust the position with the height/width of the ctx menu.
@@ -48,67 +38,7 @@ export const GraphMenu = ({ open, node, x, y }: Menu) => {
     return () => setPos(undefined);
   }, [height, width, x, y, node]);
 
-  useEffect(() => {
-    if (node) {
-      setMenuItems([
-        { name: node.properties.name, border: true, node },
-        {
-          name: "Regnskap",
-          icon: faDollarSign,
-          node,
-          condition: node.labels.includes(GraphNodeLabel.Company),
-          action: () => {
-            dispatch(setSource(node));
-            dispatch(openModal());
-            dispatch(setContent(ModalContent.Financials));
-            dispatch(closeMenu());
-          },
-        },
-        {
-          name: "Åpne i ny graf",
-          svgIcon: <GraphLogo width="16px" height="16px" />,
-          node,
-          action: () => {
-            history.push(`/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
-            dispatch(closeMenu());
-          },
-        },
-        {
-          name: "Åpne i ny fane",
-          icon: faWindowRestore,
-          node,
-          action: () => {
-            window.open(`${getBaseUrl()}/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
-            dispatch(closeMenu());
-          },
-        },
-        {
-          name: "Investeringstabell",
-          condition: !!node.labels.includes(GraphNodeLabel.Shareholder),
-          icon: faListAlt,
-          node,
-          action: () => {
-            dispatch(setSource(node));
-            dispatch(openModal());
-            dispatch(setContent(ModalContent.InvestmentTable));
-            dispatch(closeMenu());
-          },
-        },
-        {
-          name: "Investortabell",
-          condition: !!node.labels.includes(GraphNodeLabel.Company),
-          icon: faList,
-          node,
-          action: () => {
-            dispatch(setSource(node));
-            dispatch(openModal());
-            dispatch(setContent(ModalContent.InvestorTable));
-            dispatch(closeMenu());
-          },
-        },
-      ]);
-    }
-  }, [dispatch, history, node]);
+  const menuItems = useGraphMenu(node);
 
   if (!open || !node || !menuItems) return null;
 
---
+++ client/src/components/Graph2/GraphMenu/useGraphMenu.tsx
@@ -0,0 +1,81 @@
+import { faDollarSign, faList, faListAlt, faWindowRestore } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { GraphNode, GraphNodeLabel, GraphType } from "../../../models/models";
+import { closeMenu, setSource } from "../../../slices/graphSlice";
+import { ModalContent, open as openModal, setContent } from "../../../slices/modalSlice";
+import { getBaseUrl } from "../../../utils/utils";
+import { GraphLogo } from "../../GraphLogo";
+import { MenuItem } from "./GraphMenu";
+
+export const useGraphMenu = (node?: GraphNode) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [menuItems, setMenuItems] = useState<MenuItem[]>();
+
+  useEffect(() => {
+    if (node) {
+      setMenuItems([
+        { name: node.properties.name, border: true, node },
+        {
+          name: "Regnskap",
+          icon: faDollarSign,
+          node,
+          condition: node.labels.includes(GraphNodeLabel.Company),
+          action: () => {
+            dispatch(setSource(node));
+            dispatch(openModal());
+            dispatch(setContent(ModalContent.Financials));
+            dispatch(closeMenu());
+          },
+        },
+        {
+          name: "Åpne i ny graf",
+          svgIcon: <GraphLogo width="16px" height="16px" />,
+          node,
+          action: () => {
+            history.push(`/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
+            dispatch(closeMenu());
+          },
+        },
+        {
+          name: "Åpne i ny fane",
+          icon: faWindowRestore,
+          node,
+          action: () => {
+            window.open(`${getBaseUrl()}/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
+            dispatch(closeMenu());
+          },
+        },
+        {
+          name: "Investeringstabell",
+          condition: !!node.labels.includes(GraphNodeLabel.Shareholder),
+          icon: faListAlt,
+          node,
+          action: () => {
+            dispatch(setSource(node));
+            dispatch(openModal());
+            dispatch(setContent(ModalContent.InvestmentTable));
+            dispatch(closeMenu());
+          },
+        },
+        {
+          name: "Investortabell",
+          condition: !!node.labels.includes(GraphNodeLabel.Company),
+          icon: faList,
+          node,
+          action: () => {
+            dispatch(setSource(node));
+            dispatch(openModal());
+            dispatch(setContent(ModalContent.InvestorTable));
+            dispatch(closeMenu());
+          },
+        },
+      ]);
+    }
+  }, [dispatch, history, node]);
+
+  return menuItems;
+};
