3da417770499fbd0ca31b94e21159fa57eaf6871
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-08-07 15:57:17 +0200

    feat: adds menu item placeholders

--- client/src/components/Graph2/GraphMenu/useGraphMenu.tsx
+++ client/src/components/Graph2/GraphMenu/useGraphMenu.tsx
@@ -1,4 +1,13 @@
-import { faDollarSign, faList, faListAlt, faWindowRestore } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowDown,
+  faArrowUp,
+  faBuilding,
+  faDollarSign,
+  faList,
+  faListAlt,
+  faUserTie,
+  faWindowRestore,
+} from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -17,63 +26,101 @@ export const useGraphMenu = (node?: GraphNode) => {
 
   useEffect(() => {
     if (node) {
-      setMenuItems([
-        { name: node.properties.name, border: true, node },
-        {
-          name: "Regnskap",
-          icon: faDollarSign,
-          node,
-          condition: node.labels.includes(GraphNodeLabel.Company),
-          action: () => {
-            dispatch(setSource(node));
-            dispatch(openModal());
-            dispatch(setContent(ModalContent.Financials));
-            dispatch(closeMenu());
+      setMenuItems(
+        [
+          { name: node.properties.name, border: true, node },
+          {
+            name: "Regnskap",
+            icon: faDollarSign,
+            node,
+            condition: node.labels.includes(GraphNodeLabel.Company),
+            action: () => {
+              dispatch(setSource(node));
+              dispatch(openModal());
+              dispatch(setContent(ModalContent.Financials));
+              dispatch(closeMenu());
+            },
           },
-        },
-        {
-          name: "Investeringstabell",
-          condition: !!node.labels.includes(GraphNodeLabel.Shareholder),
-          icon: faListAlt,
-          node,
-          action: () => {
-            dispatch(setSource(node));
-            dispatch(openModal());
-            dispatch(setContent(ModalContent.InvestmentTable));
-            dispatch(closeMenu());
+          {
+            name: "Investeringstabell",
+            condition: !!node.labels.includes(GraphNodeLabel.Shareholder),
+            icon: faListAlt,
+            node,
+            action: () => {
+              dispatch(setSource(node));
+              dispatch(openModal());
+              dispatch(setContent(ModalContent.InvestmentTable));
+              dispatch(closeMenu());
+            },
           },
-        },
-        {
-          name: "Investortabell",
-          condition: !!node.labels.includes(GraphNodeLabel.Company),
-          icon: faList,
-          node,
-          action: () => {
-            dispatch(setSource(node));
-            dispatch(openModal());
-            dispatch(setContent(ModalContent.InvestorTable));
-            dispatch(closeMenu());
+          {
+            name: "Investortabell",
+            condition: !!node.labels.includes(GraphNodeLabel.Company),
+            icon: faList,
+            node,
+            action: () => {
+              dispatch(setSource(node));
+              dispatch(openModal());
+              dispatch(setContent(ModalContent.InvestorTable));
+              dispatch(closeMenu());
+            },
           },
-        },
-        {
-          name: "Åpne i ny graf",
-          svgIcon: <GraphLogo width="16px" height="16px" />,
-          node,
-          action: () => {
-            history.push(`/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
-            dispatch(closeMenu());
+          {
+            name: "Flere investeringer",
+            condition: !!node.labels.includes(GraphNodeLabel.Shareholder),
+            icon: faBuilding,
+            node,
+            action: () => {
+              throw Error("Not implemented.");
+            },
           },
-        },
-        {
-          name: "Åpne i ny fane",
-          icon: faWindowRestore,
-          node,
-          action: () => {
-            window.open(`${getBaseUrl()}/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
-            dispatch(closeMenu());
+          {
+            name: "Flere investorer",
+            condition: !!node.labels.includes(GraphNodeLabel.Company),
+            icon: faUserTie,
+            node,
+            action: () => {
+              throw Error("Not implemented.");
+            },
           },
-        },
-      ]);
+          {
+            name: "Flere aktører",
+            condition: node.labels.includes(GraphNodeLabel.Company) || node.labels.includes(GraphNodeLabel.Unit),
+            icon: faArrowUp,
+            node,
+            action: () => {
+              throw Error("Not implemented.");
+            },
+          },
+          {
+            name: "Flere roller",
+            condition: node.labels.includes(GraphNodeLabel.Person) || node.labels.includes(GraphNodeLabel.Unit),
+            icon: faArrowDown,
+            node,
+            action: () => {
+              throw Error("Not implemented.");
+            },
+          },
+          {
+            name: "Åpne i ny graf",
+            svgIcon: <GraphLogo width="16px" height="16px" />,
+            node,
+            action: () => {
+              history.push(`/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
+              dispatch(closeMenu());
+            },
+          },
+          {
+            name: "Åpne i ny fane",
+            icon: faWindowRestore,
+            node,
+            action: () => {
+              window.open(`${getBaseUrl()}/graph2?graphType=${GraphType.Default}&sourceUuid=${node.properties.uuid}`);
+              dispatch(closeMenu());
+            },
+          },
+        ].filter((item) => item.condition !== false)
+      );
     }
   }, [dispatch, history, node]);
 
