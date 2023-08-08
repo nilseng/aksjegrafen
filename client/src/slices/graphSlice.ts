bcf193fb1529c2982fe11ca5c0889e1bfb1ec2d1
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-08-08 11:18:38 +0200

    feat: adds RTK thunks for loading additional nodes

--- client/src/slices/graphSlice.ts
+++ client/src/slices/graphSlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { SimulationLinkDatum, SimulationNodeDatum } from "d3";
+import { toast } from "react-toastify";
 import { FetchState, GraphLink, GraphNode, GraphType } from "../models/models";
 import { buildQuery } from "../utils/buildQuery";
 
@@ -126,9 +127,124 @@ export const graphSlice = createSlice<
         if (action.error.name === "AbortError") return;
         state.status = FetchState.Error;
         state.error = action.error.message;
+      })
+      // Fetch actors cases
+      .addCase(fetchActorsThunk.pending, (_, action) => {
+        toast(`Laster aktører i ${action.meta.arg.node.properties.name}`, { type: toast.TYPE.INFO, autoClose: 1000 });
+      })
+      .addCase(fetchActorsThunk.fulfilled, (state, action) => {
+        const source = state.data.nodes.find((node) => node.properties.uuid === action.meta.arg.node.properties.uuid);
+        if (!source) {
+          throw Error(`Could not update skip, source with id=${action.meta.arg.node.properties.uuid} not found.`);
+        }
+        source.skip = source.skip
+          ? { ...source.skip, actors: (source.skip.actors += action.payload.nodes.length - 1) }
+          : { actors: action.payload.nodes.length - 1, units: 0, investments: 0, investors: 0 };
+        const newNodesCount = addToGraphIfNotExist(state, action);
+        toast(`Lastet ${newNodesCount} nye aktører i ${action.meta.arg.node.properties.name}.`, {
+          type: newNodesCount ? toast.TYPE.SUCCESS : toast.TYPE.INFO,
+        });
+      })
+      .addCase(fetchActorsThunk.rejected, (_, action) => {
+        toast(`Kunne ikke laste aktører i ${action.meta.arg.node.properties.name}`, { type: toast.TYPE.ERROR });
+      })
+      // Fetch role units cases
+      .addCase(fetchRoleUnitsThunk.pending, (_, action) => {
+        toast(`Laster rollene til ${action.meta.arg.node.properties.name}`, { type: toast.TYPE.INFO, autoClose: 1000 });
+      })
+      .addCase(fetchRoleUnitsThunk.fulfilled, (state, action) => {
+        const source = state.data.nodes.find((node) => node.properties.uuid === action.meta.arg.node.properties.uuid);
+        if (!source) {
+          throw Error(`Could not update skip, source with id=${action.meta.arg.node.properties.uuid} not found.`);
+        }
+        source.skip = source.skip
+          ? { ...source.skip, units: (source.skip.units += action.payload.nodes.length - 1) }
+          : { units: action.payload.nodes.length - 1, actors: 0, investments: 0, investors: 0 };
+        const newNodesCount = addToGraphIfNotExist(state, action);
+        toast(`Lastet ${newNodesCount} av rollene til ${action.meta.arg.node.properties.name}.`, {
+          type: newNodesCount ? toast.TYPE.SUCCESS : toast.TYPE.INFO,
+        });
+      })
+      .addCase(fetchRoleUnitsThunk.rejected, (_, action) => {
+        toast(`Kunne ikke laste rollene til ${action.meta.arg.node.properties.name}`, { type: toast.TYPE.ERROR });
+      })
+      // Fetch investors cases
+      .addCase(fetchInvestorsThunk.pending, (_, action) => {
+        toast(`Laster investorer i ${action.meta.arg.node.properties.name}`, {
+          type: toast.TYPE.INFO,
+          autoClose: 1000,
+        });
+      })
+      .addCase(fetchInvestorsThunk.fulfilled, (state, action) => {
+        const source = state.data.nodes.find((node) => node.properties.uuid === action.meta.arg.node.properties.uuid);
+        if (!source) {
+          throw Error(`Could not update skip, source with id=${action.meta.arg.node.properties.uuid} not found.`);
+        }
+        source.skip = source.skip
+          ? { ...source.skip, investors: (source.skip.investors += action.payload.nodes.length - 1) }
+          : { investors: action.payload.nodes.length - 1, actors: 0, investments: 0, units: 0 };
+        const newNodesCount = addToGraphIfNotExist(state, action);
+        toast(`Lastet ${newNodesCount} av investorene til ${action.meta.arg.node.properties.name}.`, {
+          type: newNodesCount ? toast.TYPE.SUCCESS : toast.TYPE.INFO,
+        });
+      })
+      .addCase(fetchInvestorsThunk.rejected, (_, action) => {
+        toast(`Kunne ikke laste investorene til ${action.meta.arg.node.properties.name}`, { type: toast.TYPE.ERROR });
+      })
+      // Fetch investments cases
+      .addCase(fetchInvestmentsThunk.pending, (_, action) => {
+        toast(`Laster investeringene til ${action.meta.arg.node.properties.name}`, {
+          type: toast.TYPE.INFO,
+          autoClose: 1000,
+        });
+      })
+      .addCase(fetchInvestmentsThunk.fulfilled, (state, action) => {
+        const source = state.data.nodes.find((node) => node.properties.uuid === action.meta.arg.node.properties.uuid);
+        if (!source) {
+          throw Error(`Could not update skip, source with id=${action.meta.arg.node.properties.uuid} not found.`);
+        }
+        source.skip = source.skip
+          ? { ...source.skip, investments: (source.skip.investments += action.payload.nodes.length - 1) }
+          : { investments: action.payload.nodes.length - 1, actors: 0, units: 0, investors: 0 };
+        const newNodesCount = addToGraphIfNotExist(state, action);
+        toast(`Lastet ${newNodesCount} av investeringene til ${action.meta.arg.node.properties.name}.`, {
+          type: newNodesCount ? toast.TYPE.SUCCESS : toast.TYPE.INFO,
+        });
+      })
+      .addCase(fetchInvestmentsThunk.rejected, (_, action) => {
+        toast(`Kunne ikke laste investeringene til ${action.meta.arg.node.properties.name}`, {
+          type: toast.TYPE.ERROR,
+        });
       });
   },
 });
+
+const addToGraphIfNotExist = (
+  state: GraphState,
+  action: PayloadAction<{
+    nodes: GraphNode[];
+    links: GraphLink[];
+  }>
+) => {
+  let newNodesCount = 0;
+  const currentNodeIds = new Set(state.data.nodes.map((n) => n.properties.uuid));
+  const currentLinkIds = new Set(
+    state.data.links.map((l) => `${l.source.properties.uuid}-${l.target.properties.uuid}`)
+  );
+  for (const node of action.payload.nodes) {
+    if (!currentNodeIds.has(node.properties.uuid)) {
+      state.data.nodes.push(node);
+      newNodesCount += 1;
+    }
+  }
+  for (const link of action.payload.links) {
+    if (!currentLinkIds.has(`${link.source.properties.uuid}-${link.target.properties.uuid}`)) {
+      state.data.links.push(link);
+    }
+  }
+  state.status = FetchState.Success;
+  return newNodesCount;
+};
 
 export const { setGraphType, setSourceUuid, setTargetUuid, setSource, setTarget, setIsDirected, openMenu, closeMenu } =
   graphSlice.actions;
@@ -160,7 +276,7 @@ function fetchGraph(
     skip: number;
   },
   { signal }: { signal: AbortSignal }
-) {
+): Promise<{ nodes: GraphNode[]; links: GraphLink[] }> {
   if (graphType === GraphType.Default) return fetchNeighbours({ uuid: sourceUuid, limit, skip }, { signal });
   if (graphType === GraphType.ShortestPath) {
     return fetchShortestPath({ isDirected, sourceUuid, targetUuid }, { signal });
@@ -222,6 +338,50 @@ async function fetchAllPaths(
   const query = buildQuery({ isDirected, sourceUuid, targetUuid, limit });
   const res = await fetch(`/api/graph/all-paths${query}`, { signal });
   if (!res.ok) throw Error("Beklager, noe gikk galt! Tar søket mer enn 30 sekunder, feiler det automatisk...");
+  return res.json();
+}
+
+export const fetchActorsThunk = createAsyncThunk("graph/fetchActors", fetchActors);
+
+async function fetchActors(
+  { node, limit, skip }: { node: GraphNode; limit: number; skip?: number },
+  { signal }: { signal: AbortSignal }
+): Promise<{ nodes: GraphNode[]; links: GraphLink[] }> {
+  const query = buildQuery({ uuid: node.properties.uuid, limit, skip });
+  const res = await fetch(`/api/graph/actors${query}`, { signal });
+  return res.json();
+}
+
+export const fetchRoleUnitsThunk = createAsyncThunk("graph/fetchRoleUnits", fetchRoleUnits);
+
+async function fetchRoleUnits(
+  { node, limit, skip }: { node: GraphNode; limit: number; skip?: number },
+  { signal }: { signal: AbortSignal }
+) {
+  const query = buildQuery({ uuid: node.properties.uuid, limit, skip });
+  const res = await fetch(`/api/graph/role-units${query}`, { signal });
+  return res.json();
+}
+
+export const fetchInvestorsThunk = createAsyncThunk("graph/fetchInvestors", fetchInvestors);
+
+async function fetchInvestors(
+  { node, limit, skip }: { node: GraphNode; limit: number; skip?: number },
+  { signal }: { signal: AbortSignal }
+) {
+  const query = buildQuery({ uuid: node.properties.uuid, limit, skip });
+  const res = await fetch(`/api/graph/investors${query}`, { signal });
+  return res.json();
+}
+
+export const fetchInvestmentsThunk = createAsyncThunk("graph/fetchInvestments", fetchInvestments);
+
+async function fetchInvestments(
+  { node, limit, skip }: { node: GraphNode; limit: number; skip?: number },
+  { signal }: { signal: AbortSignal }
+) {
+  const query = buildQuery({ uuid: node.properties.uuid, limit, skip });
+  const res = await fetch(`/api/graph/investments${query}`, { signal });
   return res.json();
 }
 
