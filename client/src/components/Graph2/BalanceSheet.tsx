88218dcbd508690f3e96f1ab3db65efb844589f5
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-08-14 12:19:22 +0200

    feat: includes details about equity and debt in balance sheet

--- client/src/components/Graph2/BalanceSheet.tsx
+++ client/src/components/Graph2/BalanceSheet.tsx
@@ -42,15 +42,61 @@ export const BalanceSheet = ({
         {assets.sumEiendeler?.toLocaleString(navigator?.language)}
       </code>
       <p className="font-bold my-1">Egenkapital og gjeld</p>
-      <p className="text-xs font-bold m-0">Egenkapital</p>
-      <code
-        className="text-xs mb-0"
-        style={{
-          color: theme.primary,
-        }}
-      >
-        {equityAndDebt.egenkapital.sumEgenkapital?.toLocaleString(navigator?.language)}
-      </code>
+      {(equityAndDebt?.egenkapital.innskuttEgenkapital.sumInnskuttEgenkaptial ||
+        equityAndDebt?.egenkapital.innskuttEgenkapital.sumInnskuttEgenkaptial === 0) && (
+        <>
+          <p className="text-xs font-bold m-0">Innskutt egenkapital</p>
+          <code style={{ color: theme.primary }} className="text-xs m-0">
+            {equityAndDebt?.egenkapital.innskuttEgenkapital.sumInnskuttEgenkaptial.toLocaleString(navigator?.language)}
+          </code>
+        </>
+      )}
+      {(equityAndDebt?.egenkapital.opptjentEgenkapital.sumOpptjentEgenkapital ||
+        equityAndDebt?.egenkapital.opptjentEgenkapital.sumOpptjentEgenkapital === 0) && (
+        <>
+          <p className="text-xs font-bold m-0">Opptjent egenkapital</p>
+          <code
+            style={{
+              color:
+                equityAndDebt?.egenkapital.opptjentEgenkapital.sumOpptjentEgenkapital > 0
+                  ? theme.primary
+                  : theme.danger,
+            }}
+            className="text-xs m-0"
+          >
+            {equityAndDebt?.egenkapital.opptjentEgenkapital.sumOpptjentEgenkapital.toLocaleString(navigator?.language)}
+          </code>
+        </>
+      )}
+      <div className="pb-2">
+        <p className="text-xs font-bold m-0">Egenkapital</p>
+        <code
+          className="text-xs mb-0"
+          style={{
+            color: theme.primary,
+          }}
+        >
+          {equityAndDebt.egenkapital.sumEgenkapital?.toLocaleString(navigator?.language)}
+        </code>
+      </div>
+      {(equityAndDebt?.gjeldOversikt.kortsiktigGjeld.sumKortsiktigGjeld ||
+        equityAndDebt?.gjeldOversikt.kortsiktigGjeld.sumKortsiktigGjeld === 0) && (
+        <>
+          <p className="text-xs font-bold m-0">Kortsiktig gjeld</p>
+          <code style={{ color: theme.primary }} className="text-xs m-0">
+            {equityAndDebt?.gjeldOversikt.kortsiktigGjeld.sumKortsiktigGjeld.toLocaleString(navigator?.language)}
+          </code>
+        </>
+      )}
+      {(equityAndDebt?.gjeldOversikt.langsiktigGjeld.sumLangsiktigGjeld ||
+        equityAndDebt?.gjeldOversikt.langsiktigGjeld.sumLangsiktigGjeld === 0) && (
+        <>
+          <p className="text-xs font-bold m-0">Langsiktig gjeld</p>
+          <code style={{ color: theme.primary }} className="text-xs m-0">
+            {equityAndDebt?.gjeldOversikt.langsiktigGjeld.sumLangsiktigGjeld.toLocaleString(navigator?.language)}
+          </code>
+        </>
+      )}
       <p className="text-xs font-bold m-0">Gjeld</p>
       <code
         className="text-xs mb-0"
