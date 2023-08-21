7adac992438ea99f40312e577aff66f9aab6b806
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-08-21 17:31:34 +0200

    feat: captures investor table user event

--- client/src/components/Graph2/InvestorTable.tsx
+++ client/src/components/Graph2/InvestorTable.tsx
@@ -1,22 +1,38 @@
-import { useContext, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useContext, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { AppContext } from "../../AppContext";
+import { UserEventType } from "../../models/models";
 import { useGetCompany, useInvestors } from "../../services/apiService";
 import { close } from "../../slices/modalSlice";
-import { RootState } from "../../store";
+import { captureUserEventThunk } from "../../slices/userEventSlice";
+import { RootState, useAppDispatch } from "../../store";
 import Loading from "../Loading";
 import { OwnershipTable } from "../OwnershipTable";
 
 export const InvestorTable = () => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const { theme } = useContext(AppContext);
   const { source } = useSelector<RootState, RootState["graph"]["data"]>((state) => state.graph.data);
   const { company, loading: isLoadingCompany } = useGetCompany(undefined, source?.properties.orgnr);
   const [limit] = useState(10);
   const [skip, setSkip] = useState(0);
   const { investors, loading: isLoadingInvestors } = useInvestors(company, undefined, limit, skip);
+
+  useEffect(() => {
+    if (source) {
+      dispatch(
+        captureUserEventThunk({
+          type: UserEventType.InvestorTableLoad,
+          uuid: source.properties.uuid,
+          orgnr: source.properties.orgnr,
+        })
+      );
+    }
+  }, [dispatch, source]);
+
   if (isLoadingCompany || isLoadingInvestors) return <Loading color={theme.primary} backgroundColor="transparent" />;
-  if (investors)
+
+  if (investors) {
     return (
       <div className="h-full overflow-auto mt-12">
         <h5 className="text-center text-lg pb-2">
@@ -52,5 +68,6 @@ export const InvestorTable = () => {
         </div>
       </div>
     );
+  }
   return <p>Noe ser ut til å være galt...🤔</p>;
 };
