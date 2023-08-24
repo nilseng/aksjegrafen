03a8bbb4c2d08c4d8121ef37a9bc4995ed04d256
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-08-24 18:28:27 +0200

    chore: sorts popular nodes

--- server/src/use-cases/findPopularNodes.ts
+++ server/src/use-cases/findPopularNodes.ts
@@ -1,9 +1,13 @@
 import { findNodesByUuids } from "../gateways/neo4j/neo4j.gateway";
 import { getEventCountByUuid } from "../gateways/userEvent/userEvent.gateway";
+import { GraphNode } from "../models/models";
 
 export const findPopularNodes = async () => {
   const nodeCounts = await getEventCountByUuid();
   const nodes = await findNodesByUuids({ uuids: nodeCounts.map((c) => c.uuid) });
-  // TODO: Sort nodes based on count
-  return nodes;
+  const sortedNodes: GraphNode[] = [];
+  nodeCounts.forEach((count) => {
+    sortedNodes.push(nodes.find((n) => n.properties.uuid === count.uuid)!);
+  });
+  return sortedNodes;
 };
