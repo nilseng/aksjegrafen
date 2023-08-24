85369b2f9f2f1cb73052340529fa7aa6aa8e699c
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-08-24 17:59:44 +0200

    feat: adds function to count user events by uuid

--- server/src/gateways/userEvent/userEvent.gateway.ts
+++ server/src/gateways/userEvent/userEvent.gateway.ts
@@ -7,3 +7,31 @@ export const saveUserEvent = async (event: UserEvent) => {
   if (!isUserEvent(event)) throw Error("Invalid User Event");
   await client().insertOne(event);
 };
+
+export const getEventCountByUuid = (): Promise<{ uuid: string; count: number }[]> => {
+  return client()
+    .aggregate<{ uuid: string; count: number }>([
+      {
+        $group: {
+          _id: "$uuid",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: {
+          count: -1,
+        },
+      },
+      {
+        $limit: 10,
+      },
+      {
+        $project: {
+          _id: 0,
+          uuid: "$_id",
+          count: 1,
+        },
+      },
+    ])
+    .toArray();
+};
