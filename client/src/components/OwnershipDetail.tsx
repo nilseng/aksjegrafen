6c8c62dae3325c019332b0fb7b85eb2b116e0a1b
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-09-01 23:59:41 +0200

    feat: displays stock classes in ownership table

---
+++ client/src/components/OwnershipDetail.tsx
@@ -0,0 +1,153 @@
+import { faChevronDown, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { availableYears } from "../config";
+import { useNumberFormatter } from "../hooks/useNumberFormatter";
+import { ICompany, IOwnership, IShareholder, Year } from "../models/models";
+import { NeuButton } from "./NeuButton";
+
+const getCompanyStocks = (
+  o: IOwnership,
+  year: Year,
+  stockClass?: string,
+  investor?: IShareholder,
+  investment?: ICompany
+): number | undefined => {
+  if (investor) return o.investment?.shares?.[year]?.[stockClass ?? "total"];
+  if (investment) return investment?.shares?.[year]?.[stockClass ?? "total"];
+};
+
+const getOwnershipShare = (
+  o: IOwnership,
+  year: Year,
+  stockClass: string,
+  investor?: IShareholder,
+  investment?: ICompany
+): number | null => {
+  const companyStocks = getCompanyStocks(o, year, stockClass, investor, investment);
+  if (!companyStocks) return null;
+  return (o.holdings[year]?.[stockClass ?? "total"] ?? 0) / companyStocks;
+};
+
+const getOwnershipShareText = ({
+  ownership,
+  year,
+  stockClass,
+  investor,
+  investment,
+}: {
+  ownership: IOwnership;
+  year: Year;
+  stockClass: string;
+  investor?: IShareholder;
+  investment?: ICompany;
+}): string => {
+  const share = getOwnershipShare(ownership, year, stockClass, investor, investment);
+  if (!share) return "";
+  return `${(share * 100).toFixed(1)}%`;
+};
+
+const getOwnershipChange = (o: IOwnership, year: Year, stockClass: string) => {
+  if (year === Math.min(...availableYears)) return null;
+  const cy = o.holdings[year]?.[stockClass] ?? 0;
+  const ly = o.holdings[(year - 1) as Year]?.[stockClass] ?? 0;
+  if (!cy && !ly) return null;
+  return cy - ly;
+};
+
+const getOwnershipChangeText = (
+  o: IOwnership,
+  year: Year,
+  formatNumber: (num: number) => string | undefined,
+  stockClass: string
+) => {
+  const change = getOwnershipChange(o, year, stockClass);
+  if (!change) return <p style={{ height: "1rem" }}></p>;
+  if (change > 0)
+    return (
+      <p className="text-xs text-success m-0" style={{ height: "1rem" }}>
+        +{formatNumber(change)}
+      </p>
+    );
+  return (
+    <p className="text-xs text-danger m-0" style={{ height: "1rem" }}>
+      {formatNumber(change)}
+    </p>
+  );
+};
+
+const getStockClasses = (ownership: IOwnership) => {
+  const stockClasses = new Set<string>();
+  Object.values(ownership.holdings).forEach((annualHoldings) => {
+    if (!annualHoldings) return;
+    Object.keys(annualHoldings).forEach((stockClass) => {
+      if (stockClass !== "total") stockClasses.add(stockClass);
+    });
+  });
+  return Array.from(stockClasses);
+};
+
+export const OwnershipDetail = ({
+  ownership,
+  investor,
+  investment,
+}: {
+  ownership: IOwnership;
+  investor?: IShareholder;
+  investment?: ICompany;
+}) => {
+  const formatNumber = useNumberFormatter();
+
+  const [stockClasses, setStockClasses] = useState<string[]>();
+
+  const [isDetailsVisible, setIsDetailsVisible] = useState<boolean>();
+
+  useEffect(() => {
+    setStockClasses(getStockClasses(ownership));
+  }, [ownership]);
+
+  return (
+    <>
+      {availableYears.map((year) => (
+        <div key={year} className="w-1/6 px-1 py-2">
+          {ownership.holdings[year] ? (
+            <>
+              <p className="font-bold text-xs m-0" style={{ height: "1rem" }}>
+                {getOwnershipShareText({ ownership, year, stockClass: "total", investor, investment })}
+              </p>
+              {isDetailsVisible && (
+                <>
+                  <p className="text-xs m-0" style={{ height: "1rem" }}>
+                    {ownership.holdings[year]?.total ? formatNumber(ownership.holdings[year]?.total!) : ""}
+                  </p>
+                  {getOwnershipChangeText(ownership, year, formatNumber, "total")}
+                  {stockClasses?.map((stockClass: string) => (
+                    <div key={stockClass}>
+                      {ownership.holdings[year] ? (
+                        <p className="font-bold text-xs text-ellipsis overflow-hidden">{stockClass}</p>
+                      ) : null}
+                      <p className="text-xs m-0" style={{ height: "1rem" }}>
+                        {ownership.holdings[year]?.[stockClass]
+                          ? formatNumber(ownership.holdings[year]?.[stockClass]!)
+                          : ""}
+                      </p>
+                      {getOwnershipChangeText(ownership, year, formatNumber, stockClass)}
+                    </div>
+                  ))}
+                </>
+              )}
+            </>
+          ) : null}
+        </div>
+      ))}
+      <div className="w-1/12">
+        <NeuButton
+          className="w-6 sm:w-8 h-6 sm:h-8 p-1 sm:p-2"
+          style={{ borderRadius: "100%" }}
+          textClassName="text-primary"
+          icon={isDetailsVisible ? faChevronDown : faChevronRight}
+          action={() => setIsDetailsVisible(!isDetailsVisible)}
+        />
+      </div>
+    </>
+  );
+};
--- client/src/components/OwnershipTable.tsx
+++ client/src/components/OwnershipTable.tsx
@@ -2,60 +2,9 @@ import { Fragment, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { AppContext } from "../AppContext";
 import { availableYears } from "../config";
-import { useNumberFormatter } from "../hooks/useNumberFormatter";
-import { ICompany, IOwnership, IShareholder, Year } from "../models/models";
+import { ICompany, IOwnership, IShareholder } from "../models/models";
 import { GraphLogo } from "./GraphLogo";
-
-const getCompanyStocks = (
-  o: IOwnership,
-  year: Year,
-  investor?: IShareholder,
-  investment?: ICompany
-): number | undefined => {
-  if (investor) return o.investment?.shares?.[year]?.total;
-  if (investment) return investment?.shares?.[year]?.total;
-};
-
-const getOwnershipShare = (
-  o: IOwnership,
-  year: Year,
-  investor?: IShareholder,
-  investment?: ICompany
-): number | null => {
-  const companyStocks = getCompanyStocks(o, year, investor, investment);
-  if (!companyStocks) return null;
-  return (o.holdings[year]?.total ?? 0) / companyStocks;
-};
-
-const getOwnershipShareText = (o: IOwnership, year: Year, investor?: IShareholder, investment?: ICompany): string => {
-  const share = getOwnershipShare(o, year, investor, investment);
-  if (!share) return "";
-  return `${(share * 100).toFixed(1)}%`;
-};
-
-const getOwnershipChange = (o: IOwnership, year: Year) => {
-  if (year === Math.min(...availableYears)) return null;
-  const cy = o.holdings[year]?.total ?? 0;
-  const ly = o.holdings[(year - 1) as Year]?.total ?? 0;
-  if (!cy && !ly) return null;
-  return cy - ly;
-};
-
-const getOwnershipChangeText = (o: IOwnership, year: Year, formatNumber: (num: number) => string | undefined) => {
-  const change = getOwnershipChange(o, year);
-  if (!change) return null;
-  if (change > 0)
-    return (
-      <p className="text-xs text-success m-0" style={{ height: "1rem" }}>
-        +{formatNumber(change)}
-      </p>
-    );
-  return (
-    <p className="text-xs text-danger m-0" style={{ height: "1rem" }}>
-      {formatNumber(change)}
-    </p>
-  );
-};
+import { OwnershipDetail } from "./OwnershipDetail";
 
 export const OwnershipTable = ({
   ownerships,
@@ -72,14 +21,13 @@ export const OwnershipTable = ({
 }) => {
   const { theme } = useContext(AppContext);
   const history = useHistory();
-  const formatNumber = useNumberFormatter();
   return (
     <div className="w-full flex flex-wrap pt-3 m-0">
-      <p className="w-1/3"></p>
-      <p className="w-2/3 text-center font-bold text-xs px-1 pb-2">
+      <p className="w-3/12"></p>
+      <p className="w-8/12 text-center font-bold text-xs px-1 pb-2">
         Eierandel / <span className="font-normal">Antall aksjer</span>
       </p>
-      <p className="w-1/3 text-center font-bold text-xs px-1 sm:pl-3 mb-1">Navn</p>
+      <p className="w-3/12 text-center font-bold text-xs px-1 sm:pl-3 mb-1">Navn</p>
       {availableYears.map((year) => (
         <p key={year} className="w-1/6 text-xs font-bold px-1 mb-1 sm:mb-2">
           {year}
@@ -90,8 +38,10 @@ export const OwnershipTable = ({
       </div>
       {ownerships.map((o) => (
         <Fragment key={o._id}>
-          <span className="flex justify-between items-center w-1/3 overflow-auto px-1 sm:pl-4">
-            <p className="text-xs m-0">{investment ? o.investor?.shareholder?.name : o.investment?.name}</p>
+          <span className="flex justify-between items-center w-3/12 overflow-auto px-1 sm:pl-4">
+            <p className="text-xs text-ellipsis overflow-hidden m-0">
+              {investment ? o.investor?.shareholder?.name : o.investment?.name}
+            </p>
             {getGraphLink ? (
               <span
                 className="flex justify-center items-center ml-2 mr-sm-4"
@@ -114,17 +64,7 @@ export const OwnershipTable = ({
               </span>
             ) : null}
           </span>
-          {availableYears.map((year) => (
-            <div key={year} className="w-1/6 overflow-auto px-1 py-2">
-              <p className="font-bold text-xs m-0" style={{ height: "1rem" }}>
-                {getOwnershipShareText(o, year, investor, investment)}
-              </p>
-              <p className="text-xs m-0" style={{ height: "1rem" }}>
-                {o.holdings[year]?.total ? formatNumber(o.holdings[year]?.total!) : ""}
-              </p>
-              {getOwnershipChangeText(o, year, formatNumber)}
-            </div>
-          ))}
+          <OwnershipDetail ownership={o} investment={investment} investor={investor} />
           <div className="w-full px-1 sm:px-3 pb-1 sm:pb-3">
             <div className="w-full" style={{ borderBottom: `0.5px solid ${theme.primary}` }}></div>
           </div>
