83ed3fec5e9022124bda5c9c0b48e9e49d79732e
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-09-03 19:57:45 +0200

    feat: adds orgnr to unit info

--- client/src/components/Graph2/UnitInformation.tsx
+++ client/src/components/Graph2/UnitInformation.tsx
@@ -9,8 +9,9 @@ export const UnitInformation = () => {
     <div className="flex flex-col h-full w-full overflow-auto mt-12 px-2">
       <h1 className="font-bold text-center">{unit?.navn}</h1>
       {unit?.konkurs && <p className="text-danger">Konkurs</p>}
+      {unit?.organisasjonsnummer && <code className="text-center text-xs text-muted">{unit?.organisasjonsnummer}</code>}
       {unit?.hjemmeside && (
-        <code className="text-center pb-4">
+        <code className="text-center text-xs font-bold text-primary pb-4">
           <a
             href={/^https?:\/\//.test(unit.hjemmeside) ? unit?.hjemmeside : `https://${unit.hjemmeside}`}
             target="_blank"
