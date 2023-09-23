3b8c1946c08c6fb4e06d08dc58bd9c0d4e2e748e
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-09-23 15:08:17 +0200

    fix: standardizes array query params

--- client/src/utils/buildQuery.ts
+++ client/src/utils/buildQuery.ts
@@ -1,9 +1,15 @@
 import { isEmpty } from "lodash";
 
-export const buildQuery = (o?: { [key: string]: string | number | boolean | undefined }) => {
+export const buildQuery = (o?: { [key: string]: string | number | boolean | string[] | undefined }) => {
   if (!o || isEmpty(o)) return "";
   return `?${Object.keys(o)
-    .map((key) => (o[key] || o[key] === false ? `${key}=${o[key]}` : ""))
+    .map((key) => getParam({ key, value: o[key] }))
     .filter((str) => !!str)
     .join("&")}`;
+};
+
+const getParam = ({ key, value }: { key: string; value?: string | number | boolean | string[] }): string => {
+  if (!value && value !== false) return "";
+  if (Array.isArray(value)) return value.map((i) => `${key}=${i}`).join("&");
+  return `${key}=${value}`;
 };
