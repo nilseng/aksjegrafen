990244d17ad912fdf918898b3db24f37e2eec1cd
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-09-24 20:47:48 +0200

    feat: displays circular link in case node owns stock in itself

--- client/src/components/Graph2/GraphLink.tsx
+++ client/src/components/Graph2/GraphLink.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { AppContext } from "../../AppContext";
 import { GraphLinkType, GraphLink as IGraphLink } from "../../models/models";
 import { RootState } from "../../store";
+import { graphConfig } from "./GraphConfig";
 
 const formatNumber = (num: number) => (num >= 100 ? format(".3s")(num) : format(".0f")(num));
 
@@ -11,6 +12,59 @@ export const GraphLink = ({ link }: { link: IGraphLink }) => {
   const { theme } = useContext(AppContext);
 
   const roleTypes = useSelector<RootState, RootState["roles"]["data"]>((state) => state.roles.data);
+
+  if (link.source.properties.uuid === link.target.properties.uuid)
+    return (
+      <>
+        <circle
+          className="graph-circle-link"
+          fill="transparent"
+          stroke={theme.primary}
+          strokeWidth="1"
+          cx={graphConfig.nodeDimensions.width / 2}
+          cy={0}
+          r="40"
+        />
+        <g className="graph-link-arrow">
+          <line
+            x1={graphConfig.nodeDimensions.width / 2 + 35}
+            y1={-5}
+            x2={graphConfig.nodeDimensions.width / 2 + 40}
+            y2={0}
+            stroke={theme.primary}
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+          <line
+            x1={graphConfig.nodeDimensions.width / 2 + 40}
+            y1={0}
+            x2={graphConfig.nodeDimensions.width / 2 + 45}
+            y2={-5}
+            stroke={theme.secondary}
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+          <foreignObject
+            className="text-primary text-xs text-center"
+            width={200}
+            height={50}
+            transform={`translate(${graphConfig.nodeDimensions.width / 2 + 4}, -12)`}
+          >
+            {link.type !== GraphLinkType.OWNS && (
+              <p className="font-bold">
+                {roleTypes?.find((role) => role.kode === link.type)?.beskrivelse ?? link.type}
+              </p>
+            )}
+            {link.properties.stocks && (
+              <p>
+                <span className="font-bold pr-1">{((link.properties.share ?? 0) * 100).toFixed(0)}%</span>(
+                {formatNumber(link.properties.stocks)} aksjer){" "}
+              </p>
+            )}
+          </foreignObject>
+        </g>
+      </>
+    );
 
   return (
     <>
