a7eed2bd3fe7ae714af83c66ef53e321a085d1b7
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-10-06 21:52:58 +0200

    chore: deletes legacy table context

--- client/src/AppContext.tsx
+++ client/src/AppContext.tsx
@@ -1,21 +1,10 @@
-import { createContext, Dispatch, SetStateAction } from "react";
-import { ICompany, IShareholder } from "./models/models";
+import { createContext } from "react";
 import { Theme, theming } from "./theming/theme";
 
 interface IAppContext {
   theme: typeof theming.light;
-  tableModalInput: {
-    investment?: ICompany;
-    setInvestment: Dispatch<SetStateAction<ICompany | undefined>>;
-    investor?: IShareholder;
-    setInvestor: Dispatch<SetStateAction<IShareholder | undefined>>;
-    limit: number;
-    skip: number;
-    setSkip: Dispatch<SetStateAction<number>>;
-  };
 }
 
 export const AppContext = createContext<IAppContext>({
   theme: theming[Theme.light],
-  tableModalInput: { setInvestment: () => {}, setInvestor: () => {}, limit: 10, skip: 0, setSkip: () => {} },
 });
