bde77f9dc6ebe6264f7bb0e071d94b25b0b0d74f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2024-09-14 12:56:43 +0200

    chore: increases batch size when generating graph node uuids (some role nodes are created without)

--- server/src/use-cases/generateGraphNodeUUIDs.ts
+++ server/src/use-cases/generateGraphNodeUUIDs.ts
@@ -4,9 +4,13 @@ export const generateGraphNodeUUIDs = async (graphDB: Driver) => {
   const session = graphDB.session();
   const res = await session.run(`
         CALL apoc.periodic.iterate(
-            'MATCH (n) RETURN n',
+            '
+            MATCH (n) 
+            WHERE n.uuid IS NULL 
+            RETURN n
+            ',
             'SET n.uuid = randomUUID()',
-            {batchSize: 10000, paralell: true}
+            {batchSize: 50000, paralell: true}
         )
     `);
   console.log(res.records);
