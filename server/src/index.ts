3ce043dc3750f346b6fb0ae9f85528661931d83a
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2025-05-27 20:02:24 +0200

    chore: cleans up import related code from server index

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,67 +5,13 @@ import sslRedirect from "heroku-ssl-redirect";
 import morgan from "morgan";
 import { Driver } from "neo4j-driver";
 import path from "path";
-import { hideBin } from "yargs/helpers";
-import yargs from "yargs/yargs";
+
 import { Database } from "./database/databaseSetup";
-import { Year } from "./models/models";
 import { api } from "./routes/api";
 import brregRouter from "./routes/brreg";
 import { businessCodeRoutes } from "./routes/businessCodes";
-import { importData } from "./services/importService";
 
 dotenv.config();
-
-const argv = yargs(hideBin(process.argv))
-  .options({
-    import: { type: "boolean", default: false, description: "Run the unified import flow when starting the server" },
-    transform: { type: "boolean", default: false, description: "Run data transformation when starting the server" },
-    year: {
-      type: "number",
-      description: "Specify year for data import (2015-2024)",
-      demandOption: true,
-      coerce: (arg) => {
-        const validYears = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
-        if (!validYears.includes(arg)) {
-          throw new Error(`Year must be one of: ${validYears.join(", ")}`);
-        }
-        return arg;
-      },
-    },
-    importToMongoDB: {
-      type: "boolean",
-      default: true,
-      description: "Import shareholder registry to MongoDB",
-    },
-    runTransformation: {
-      type: "boolean",
-      default: true,
-      description: "Transform data (count stocks, investor counts, etc.)",
-    },
-    importToGraph: {
-      type: "boolean",
-      default: true,
-      description: "Import data from MongoDB to Neo4j graph database",
-    },
-
-    generateUUIDs: {
-      type: "boolean",
-      default: false,
-      description: "Generate UUIDs for graph nodes",
-    },
-    importBusinessCodes: {
-      type: "boolean",
-      default: false,
-      description: "Import business codes",
-    },
-    importRoles: {
-      type: "boolean",
-      default: false,
-      description: "Import roles",
-    },
-  })
-  .help()
-  .parseSync();
 
 const app = express();
 
@@ -108,17 +54,6 @@ const initializeApp = async () => {
     console.error(err.stack);
     return res.status(500).json({ error: "An unexpected error occured." });
   });
-
-  if (argv.import) {
-    importData(graphDB, db, argv.year as Year, {
-      importToMongoDB: argv.importToMongoDB as boolean,
-      runTransformation: argv.runTransformation as boolean,
-      importToGraph: argv.importToGraph as boolean,
-      generateUUIDs: argv.generateUUIDs as boolean,
-      importBusinessCodes: argv.importBusinessCodes as boolean,
-      importRoles: argv.importRoles as boolean,
-    });
-  }
 
   gracefulShutdown({ graphDB });
 };
